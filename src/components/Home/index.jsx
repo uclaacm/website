b148ee4bc3fed209407eea9d62bc8544699a481d
Nikhil Kansal <nkansal96@gmail.com>
2017-09-17 20:32:18 -0700

    added initial home banner

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,6 +7,12 @@ export default class Home extends React.Component {
 		return (
 			<div>
 				<Navbar />
+				<div className="banner">
+					<div className="banner-content right">
+						<h1>code the future.</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit, orci in pellentesque congue, eros leo pellentesque diam, in rutrum sem urna nec libero.</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
