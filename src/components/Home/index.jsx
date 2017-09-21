d1ca3ecf8943cfad3afb4cf0af9da672a5e1ebc8
Nikhil Kansal <nkansal96@gmail.com>
2017-09-21 14:22:02 -0700

    center button

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -19,7 +19,7 @@ export default class Home extends React.Component {
 					<p className="subheading">ACM is the largest computer science student organization at UCLA. We welcome students of all different backgrounds, interests, and skill levels to join our community and share in our passion for CS. ACM is split into six committees &mdash; each serving a different topic and mission. Although some events "belong" to a committee, being a member of a committee is the same as being a member of ACM overall. Our events are open to everyone.</p>
 				</div>
 				<Committees committees={Config.committees} />
-				<div className="button-section">
+				<div className="button-section center">
 					<Button text="Learn More" />
 				</div><br />
 				<Carousal images={Config.carousal.images} />
