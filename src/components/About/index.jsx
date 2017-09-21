d7175808287d1b43dfe8bd0f4c5b0b71057acd24
Nikhil Kansal <nkansal96@gmail.com>
2017-09-21 15:03:49 -0700

    updates to about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,16 +13,29 @@ export default class About extends React.Component {
 				<Navbar />
 				<Banner decorative />
 				<div className="content-section">
-					<h1>What is ACM?</h1>
+					<div className="square-ornament">
+						<div className="square-splash" />
+						<div className="square-small" />
+						<div className="square-tiny" />
+					</div>
 					<div className="text-section">
+						<h1>What is ACM?</h1>
 						<p className="half-size">ACM is the largest computer science student organization at UCLA. We welcome students of all different backgrounds and skill levels to join our community and share in our passion for CS!</p>
 						<p className="half-size">From infosessions to workshops to hackathons and competitions, our events cover a variety of topics including artificial intelligence, virtual reality, security, beginner workshops, diversity initiatives, community outreach, and more.</p>
 						<p className="half-size">At UCLA ACM, we aim to develop students’ technical and professional skills while cultivating an inclusive and supportive community for all those interested in computer science.</p>
 					</div>
-					<div className="shadow-square">
+				</div>
+				<div className="content-section right">
+					<div className="square-ornament ornament-left">
 						<div className="square-splash" />
 						<div className="square-small" />
 						<div className="square-tiny" />
+					</div>
+					<div className="text-section">
+						<h1>What is ACM?</h1>
+						<p className="half-size">ACM is the largest computer science student organization at UCLA. We welcome students of all different backgrounds and skill levels to join our community and share in our passion for CS!</p>
+						<p className="half-size">From infosessions to workshops to hackathons and competitions, our events cover a variety of topics including artificial intelligence, virtual reality, security, beginner workshops, diversity initiatives, community outreach, and more.</p>
+						<p className="half-size">At UCLA ACM, we aim to develop students’ technical and professional skills while cultivating an inclusive and supportive community for all those interested in computer science.</p>
 					</div>
 				</div>
 				<Footer />
