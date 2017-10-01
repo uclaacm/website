bed2889609542e13a7073b14b068afc697afd12e
Nikhil Kansal <nkansal96@gmail.com>
2017-09-30 19:21:06 -0700

    update sponsors

--- src/config/sponsors.jsx
+++ src/config/sponsors.jsx
@@ -1,31 +1,51 @@
 export default [
 	{
-		'name': 'Palantir',
-		'image': '/images/sponsors/palantir.png',
+		'name': 'American Express',
+		'image': '/images/sponsors/amex.svg',
+	},
+	{
+		'name': 'Blizzard',
+		'image': '/images/sponsors/blizzard.png',
+	},
+	{
+		'name': 'Bloomberg',
+		'image': '/images/sponsors/bloomberg.svg',
+	},
+	{
+		'name': 'Facebook',
+		'image': '/images/sponsors/facebook.svg',
 	},
 	{
 		'name': 'Google',
 		'image': '/images/sponsors/google.svg',
 	},
 	{
-		'name': 'Microsoft',
-		'image': '/images/sponsors/microsoft.svg',
+		'name': 'KPMG',
+		'image': '/images/sponsors/kpmg.png',
 	},
 	{
-		'name': 'Cisco Systems',
-		'image': '/images/sponsors/cisco.svg',
+		'name': 'Northrop Grumman',
+		'image': '/images/sponsors/northrop-grumman.png',
 	},
 	{
-		'name': 'BlackRock',
-		'image': '/images/sponsors/blackrock.png',
+		'name': 'Qualcomm',
+		'image': '/images/sponsors/qualcomm.svg',
 	},
 	{
-		'name': 'Tesoro',
-		'image': '/images/sponsors/tesoro.svg',
+		'name': 'Sandia National Laboratories',
+		'image': '/images/sponsors/snl.png',
 	},
 	{
-		'name': 'Redfin',
-		'image': '/images/sponsors/redfin.png',
+		'name': 'Strategic Global Advisors',
+		'image': '/images/sponsors/sgallc.jpg',
+	},
+	{
+		'name': 'Symantec',
+		'image': '/images/sponsors/symantec.jpg',
+	},
+	{
+		'name': 'Teradata',
+		'image': '/images/sponsors/teradata.jpg',
 	},
 	{
 		'name': 'Uber',
