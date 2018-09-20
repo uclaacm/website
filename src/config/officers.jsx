a9149c9d5672f325aa1b7b70776f2865cfb1e611
Nikhil Kansal <nkansal96@gmail.com>
2018-09-20 09:50:56 -0700

    update officers

--- src/config/officers.jsx
+++ src/config/officers.jsx
@@ -1,47 +1,56 @@
 export default [
 	{
-		name: 'Mihir Mathur',
-		title: 'President',
-		image: '/images/officers/mihir.jpg',
+		name: "Yvonne Chen",
+		title: "President",
+		email: "ycyvonne@ucla.edu",
+		image: "/images/officers/yvonne.jpg"
 	},
 	{
-		name: 'Nikhil Kansal',
-		title: 'Vice President',
-		image: '/images/officers/nikhil.jpg',
+		name: "Nathan Yang",
+		title: "External VP",
+		email: "thenathanyang@ucla.edu",
+		image: "/images/officers/nathan.jpg"
 	},
 	{
-		name: 'Jennifer Liaw',
-		title: 'Vice President',
-		image: '/images/officers/jennifer.jpg',
+		name: "Natasha Kohli",
+		title: "Internal VP",
+		email: "jdliaw@ucla.edu",
+		image: "/images/officers/natasha.jpg"
 	},
 	{
-		name: 'Rohan Varma',
-		title: 'AI President',
-		image: '/images/officers/rohan.jpg',
+		name: "Adit Deshpande",
+		title: "President of AI",
+		email: "rvarm1@gmail.com",
+		image: "/images/officers/adit.jpg"
 	},
 	{
-		name: 'Yvonne Chen',
-		title: 'Hack President',
-		image: '/images/officers/yvonne.jpg',
+		name: "Kevin Tan",
+		title: "President of Hack",
+		email: "rvarm1@gmail.com",
+		image: "/images/officers/kevin.jpg"
 	},
 	{
-		name: 'Jerry Li',
-		title: 'ICPC President',
-		image: '/images/officers/jerry.jpg',
+		name: "Jeffrey Zhao",
+		title: "President of ICPC",
+		email: "jerrylinew@gmail.com",
+		image: "/images/officers/jeffrey.jpg"
 	},
 	{
-		name: 'Tyler Lindberg',
-		title: 'Studio President',
-		image: '/images/officers/tyler.jpg',
+		name: "Akshara S.",
+		title: "President of Cyber",
+		email: "alrehanitanya@gmail.com",
+		image: "/images/officers/akshara.jpg"
 	},
 	{
-		name: 'Tanya Al-Rehani',
-		title: 'NetSec President',
-		image: '/images/officers/tanya.jpg',
+		name: "Judy Kim",
+		title: "President of Studio",
+		email: "judyapplekim@gmail.com",
+		image: "/images/officers/judy.jpg"
 	},
 	{
-		name: 'Garima Lunawat',
-		title: 'ACM-W President',
-		image: '/images/officers/garima.jpg',
+		name: "Caroline Quigg",
+		title: "President of ACM-W",
+		email: "glunawat@ucla.edu",
+		image: "/images/officers/caroline.jpg"
 	},
 ];
