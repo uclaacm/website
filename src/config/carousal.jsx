c6b9c46beaa76e92f2fbb00b6f1cfed1d26c39b7
alexanderqchen <aqchen@ucla.edu>
2018-10-05 22:45:34 -0700

    Updated carousel image links to static files

--- src/config/carousal.jsx
+++ src/config/carousal.jsx
@@ -1,20 +1,20 @@
 export default {
 	images: [
-		'https://lh3.googleusercontent.com/JauKWk2AUKCa79nWcS5o8bejREu-yu2ODLJTcxlA1d6OZDP2hI2e7ZUaIigJcijStRsspJkCKMxbNqhwJhMf=w1319-h617-rw',
-		'https://lh4.googleusercontent.com/YvDH6uG4Z9bpU69RJKLj7-2rj6g5zV5J55k9Oz9flhYQ6DFyM1_MdywiB413VqRBtrTFZf9wbIhWbxdOgWKa=w1319-h666-rw',
-		'https://lh6.googleusercontent.com/9HO1ImfqhUk1XH2EBgxbkkZcasvOC3owxttUg2jDQjtdwyuXkQU_62BouaEEqb_Q_YYx7Rc3lMvdMhXFjO3G=w1319-h666-rw',
-		'https://lh5.googleusercontent.com/spNHx0hsQ_Op6zBtxdEh6zHw6DgGXtHlvlvbNlU0Sc6nmbG6IeCH2uQRJ_fIW6MjXg4fbMFXb-alTPguTyT2=w1319-h666-rw',
-		'https://lh6.googleusercontent.com/RlGYoz34H7DRfvZOELVadW7qjoexVDyKUAVAvpnk0VsFobJwIYIcauL_prKX9qS__DYDfxgvEaqlHM6u8HSf=w1319-h666-rw',
-		'https://lh5.googleusercontent.com/5_31MYP3bOl6KtPkBuwhX8B6Qb8CpGOy_ynYfpTbG7JD4bqVM0z0HI9Evb442U0vgx-mweIiZx-RmLwW3isf=w1319-h666-rw',
-		'https://lh3.googleusercontent.com/4ZUH2ZcMeZZY7yC3WfExR-PDaRMl1RM8p7l0Hkmn1sOUd1npNuJmxru8hedM7o8lvEfsbR-IOboCxum0pSoz=w1319-h666-rw',
-		'https://lh4.googleusercontent.com/iQHlJg1VFL9Lwze4fk3mFwgVwKzDkYPO2Wa5xn8aRjSX-6t85EXsCu6u3WcgwzJhFCbOu6Ek6LkGufEQkkxY=w1319-h666-rw',
-		'https://lh3.googleusercontent.com/5YQSlmuQjwyG4PqfUuxm50ubJFAFfmKC8KWkbLm3mdctrYlpA4OHohfSjG7c-6G_nJfsWRMmBmQvA6DE0ZDv=w1319-h666-rw',
-		'https://lh4.googleusercontent.com/WyIyPvkRpxXFf6pYPDDrYi3wj33FXUgiWjYQQBy4dx_LUgRcQh5871nEhSWRHVoqL5xVxs5_-9ECi58l_0-E=w1319-h666-rw',
-		'https://lh6.googleusercontent.com/nDhd6ayqLdNwVtK1Xd7KudWpFmZeOM9LuMQNUNfQ-za-yu8g68y_SfcUTHjoh-t1eHIKETGBFUXlL88upZAu=w1319-h666-rw',
-		'https://lh6.googleusercontent.com/wTer8xdotG53xI7nt1Kk12TPcfdYcE32NHrZ7c9XZ5IrDPlrfFcAw8zgekDWOFbXN2TBqmO6XnubXKGTeqGK=w1319-h666-rw',
-		'https://lh5.googleusercontent.com/tHPMDfqGx9SMgQ1xAng1rsfIV4NBEKkcApe-w8Lz7daqoUQLZCzZ1zvcMeWKeTQwdbhNkQXCEu0273RsTNzM=w1319-h666-rw',
-		'https://lh6.googleusercontent.com/CUh_JM4r4fSM0gvkHKI9T8qwWO6oXi0d_UL5zX8NpZrq1U5ZIoXVj-inHjXL5DUD53MK5ztpvru8Oqr9_LMw=w1319-h666-rw',
-		'https://lh3.googleusercontent.com/PYYzShhrckDEfpTXKniJ-IH5O1PCum97tBGDHIR-ZYRS5yTbdduFvlF9zhsv-P0fXlU47nY1-UjqZXdJ-FR-=w1319-h666-rw',
-		'https://lh5.googleusercontent.com/HmjUkeJ4t4xXnGaIdFy1b_-Jzp_cb-r_X40CkU9KwlD8Y00C-9MwArPnC72xU_SpD-0CRtBDFQUI-5gC7wr5=w1319-h666-rw'		
+		'/images/carousal/01.jpg',
+		'/images/carousal/02.jpg',
+		'/images/carousal/03.jpg',
+		'/images/carousal/04.jpg',
+		'/images/carousal/05.jpg',
+		'/images/carousal/06.jpg',
+		'/images/carousal/07.jpg',
+		'/images/carousal/08.jpg',
+		'/images/carousal/09.jpg',
+		'/images/carousal/10.jpg',
+		'/images/carousal/11.jpg',
+		'/images/carousal/12.jpg',
+		'/images/carousal/13.jpg',
+		'/images/carousal/14.jpg',
+		'/images/carousal/15.jpg',
+		'/images/carousal/16.jpg'
 	]
 }
