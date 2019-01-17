390b23f71ab1fee6da8754269aafc128440b5171
Ning Hu <ning.hu1205@gmail.com>
2019-01-16 17:29:11 -0800

    history page

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -2,10 +2,12 @@ import Committees from './committees';
 import Carousal from './carousal';
 import Sponsors from './sponsors';
 import Officers from './officers';
+import History from './history';
 
 export default {
 	committees: Committees,
 	carousal: Carousal,
 	sponsors: Sponsors,
 	officers: Officers,
+	history: History,
 };
---
+++ src/containers/history.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import HistoryComponent from 'components/History';
+
+export default class History extends React.Component {
+	render() {
+		return <HistoryComponent />;
+	}
+}
