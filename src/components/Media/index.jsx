b6fd5c8bc92a8ffb15b58e09914710fe7d141764
Disha Zambani <dmzambani@gmail.com>
2019-01-23 09:09:16 -0800

    added media tab and page

---
+++ src/components/Media/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Config from 'config';
+
+import Navbar from 'components/Navbar';
+import Button from 'components/Button';
+import Footer from 'components/Footer';
+import Banner from 'components/Banner';
+
+export default class Media extends React.Component {
+	render() {
+		return (
+			<div className="media-page">
+				<Navbar />
+				<Banner decorative />
+				<div className="content-section center">
+					<h2>Our Media</h2>
+					<p className="subheading">Rad pics.</p>
+						{Config.media.map(media =>
+							<p> {media.name} </p>
+				</div>
+				<Footer />
+			</div>
+		);
+	}
+}
---
+++ src/containers/media.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+import MediaComponent from 'components/Media';
+
+export default class Media extends React.Component {
+	render() {
+		return <MediaComponent />;
+	}
+}
