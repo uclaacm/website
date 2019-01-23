56950b8af5cc6c3eb740f4fc7988f868dfc07ca4
Disha Zambani <dmzambani@gmail.com>
2019-01-23 14:36:59 -0800

    updated mediapage

--- src/components/Media/index.jsx
+++ src/components/Media/index.jsx
@@ -17,6 +17,7 @@ export default class Media extends React.Component {
 					<p className="subheading">Rad pics.</p>
 						{Config.media.map(media =>
 							<p> {media.name} </p>
+						)}
 				</div>
 				<Footer />
 			</div>
---
+++ src/config/media.jsx
@@ -0,0 +1,17 @@
+export default [
+	{
+		name: 'New Committees!',
+		date: '',
+		blurb: 'design',
+	},
+	{
+		name: 'HOTH',
+		date: '',
+		blurb: 'a good time for some good memes'
+	},
+	{
+		name: 'Rad Pics',
+		date: '',
+		blurb: '',
+	}
+]
