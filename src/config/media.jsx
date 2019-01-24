5e1c358e48f9b359a3d884e5d83a16190e76cd04
Disha Zambani <44249551+dzambani@users.noreply.github.com>
2019-01-23 17:41:29 -0800

    created unstyled media page (#25)
    * minify images, update officer style
    
    * added media tab and page
    
    * updated mediapage
    
    * Update media.jsx
    
    * Update index.jsx
    
    * Update index.jsx
    
    "cOdE sTyLiNg"
    
    * Update index.jsx
    
    "no spaces before and after tags"
    
    * Update index.jsx
    
    fixed mistake that displayed className on page

---
+++ src/config/media.jsx
@@ -0,0 +1,17 @@
+export default [
+	{
+		name: 'New Committees!',
+		date: '',
+		tagline: 'design',
+	},
+	{
+		name: 'HOTH',
+		date: '',
+		tagline: 'a good time for some good memes'
+	},
+	{
+		name: 'Rad Pics',
+		date: '',
+		tagline: '',
+	}
+]
