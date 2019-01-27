7aa30b3a2d95727c166ed620347b3c422b7283d4
Sabrina Liu <liusabrina22@gmail.com>
2019-01-26 16:48:03 -0800

    added a vertical line

--- src/components/History/Timeline/historytile.jsx
+++ src/components/History/Timeline/historytile.jsx
@@ -4,14 +4,14 @@ export default class HistoryTile extends React.Component {
 	render() {
 		return (
 			<div className='history-tile'>
-                <div className='history-date'>
-                    <p>{this.props.history.date}</p>
-                </div>
-                <div className='history-info'>
-                    <h2>{this.props.history.title}</h2>
-                    <p>{this.props.history.description}</p>
-                </div>
-            </div>
-        );
+				<div className='history-date'>
+					<p>{this.props.history.date}</p>
+				</div>
+				<div className='history-info'>
+					<h2>{this.props.history.title}</h2>
+					<p>{this.props.history.description}</p>
+				</div>
+			</div>
+		);
 	}
 }
