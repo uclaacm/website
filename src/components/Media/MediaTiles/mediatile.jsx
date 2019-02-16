a89f40afd0a53846acd930ba12aa27f2429ccf5e
Disha Zambani <dmzambani@gmail.com>
2019-02-15 16:57:10 -0800

    partially styled mediaTiles update

--- src/components/Media/MediaTiles/mediatile.jsx
+++ src/components/Media/MediaTiles/mediatile.jsx
@@ -3,14 +3,16 @@
 export default class MediaTile extends React.Component {
 	render() {
 		return (
+			<a href={this.props.media.link}>
 			<div className= "media" style= {{backgroundImage: 'url(' + this.props.media.image + ')'}}>
 				<h2><a href={this.props.media.link}>{this.props.media.title}</a></h2>
-				<p>{this.props.media.quote[0].content}</p>
+				<p>"{this.props.media.quote[0].content}"</p>
 				<div className= "speaker">
-					<p>{this.props.media.quote[0].speaker}</p>
+					<p>-{this.props.media.quote[0].speaker}</p>
 					<p>{this.props.media.quote[0].position}</p>
 				</div>
 			</div>
+			</a>
 		);
 	}
 }
