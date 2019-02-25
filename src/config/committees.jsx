5147c95d8af5749e27997fd2523486b82ce47eca
Sabrina Liu <liusabrina22@gmail.com>
2019-02-25 11:09:23 -0800

    added descriptions to committees.jsx

--- src/config/committees.jsx
+++ src/config/committees.jsx
@@ -5,6 +5,8 @@ export default [
 		tagline: 'Move Fast, Build Things',
 		image: '/images/committees/logo-hack.svg',
 		link: 'https://www.facebook.com/groups/acmhack/',
+		description: 'Hack is a committee dedicated to helping students build amazing things and encouraging them to explore the limits of what is possible through code. Hack\'s events range from coding bootcamps for web and mobile dev to beginner-friendly all-day hackathons.',
+
 	},
 	{
 		name: 'W',
@@ -12,6 +14,8 @@ export default [
 		tagline: 'Starting Conversations about Diversity in Tech',
 		image: '/images/committees/logo-w.svg',
 		link: 'https://www.facebook.com/groups/uclaacmw',
+		description: 'ACM-W supports, celebrates, and advocates for the full engagement of all who are interested in computer science. Additionally, ACM-W endeavors to increase all aspects of diversity in the technical field by providing a range of programs and services to UCLA students.',
+
 	},
 	{
 		name: 'AI',
@@ -19,6 +23,8 @@ export default [
 		tagline: 'Learning Machine Learning',
 		image: '/images/committees/logo-ai.svg',
 		link: 'https://uclaacmai.github.io/',
+		description: 'ACM AI is focused on creating and developing a community of UCLA students who are interested in artificial intelligence and machine learning. Our group hopes to achieve this by teaching AI/ML through workshops, coordinating internal and external projects, as well as hosting events that inspire and cultivate interest in the field.',
+
 	},
 	{
 		name: 'Studio',
@@ -26,6 +32,8 @@ export default [
 		tagline: 'Creative Expression through Games and VR',
 		image: '/images/committees/logo-studio.svg',
 		link: 'https://www.facebook.com/groups/uclaacmstudio',
+		description: 'ACM Game Studio is focused on using games and VR as a medium of creativity and expression. We want to help students learn the tools and skills necessary to create exciting experiences as well as providing a space to find teammates to work on year-long projects.',
+
 	},
 	{
 		name: 'ICPC',
@@ -33,6 +41,8 @@ export default [
 		tagline: 'Mastering Algorithms and Competitive Programming',
 		image: '/images/committees/logo-icpc.svg',
 		link: 'https://www.facebook.com/groups/ucla.icpc',
+		description: 'test',
+
 	},
 	{
 		name: 'Cyber',
@@ -40,19 +50,25 @@ export default [
 		tagline: 'Cybersecurity Made Simple',
 		image: '/images/committees/logo-cyber.svg',
 		link: 'https://www.facebook.com/groups/320869398280511',
+		description: 'ACM Cyber is UCLA’s primary cybersecurity club. Our goal is to make cybersecurity more accessible and form a community of people passionate about learning about it. We run workshops starting from introductory Computer Science concepts that walk members through cyber security methods and tools, many of which are aimed to prepare students for the yearly California CTF, and by extension to potential security careers.',
+
 	},
 	{
 		name: 'Design',
 		class: 'design',
 		tagline: 'Form Follows Function',
 		image: '/images/committees/logo-design.svg',
-		link: 'https://www.facebook.com/uclaacm/'
+		link: 'https://www.facebook.com/uclaacm/',
+		description: 'The goal of Design is to manage and develop ACM\'s visual identity, promote ACM as an organization, as well as provide a platform to introduce students to design in the tech community at UCLA. We create marketing materials for ACM events and provide UCLA students an introduction to design and its role in the field of Computer Science.',
+
 	},
 	{
 		name: 'Teach LA',
 		class: 'teachla',
 		tagline: 'Learn It, Teach It',
 		image: '/images/committees/logo-teachla.svg',
-		link: 'https://www.facebook.com/teachlaucla/'
+		link: 'https://www.facebook.com/teachlaucla/',
+		description: 'TeachLA is an ACM committee attempting to assist schools across Los Angeles build their computer science curriculums. We do so by connecting student volunteers with local elementary, middle, and high schools that would like to explore a programming education, but would otherwise not have the chance. We recruit every quarter, so be sure to follow our FB page for more updates!',
+
 	}
 ]
