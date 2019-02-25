c9a137ba212d408d77983b3b3c4668d7a9d974ce
Sabrina Liu <liusabrina22@gmail.com>
2019-02-25 10:48:34 -0800

    fixed css of descriptions

--- src/components/CommitteeDescriptions/Descriptions/description.jsx
+++ src/components/CommitteeDescriptions/Descriptions/description.jsx
@@ -4,20 +4,14 @@ export default class Description extends React.Component {
 	render() {
 		return (
 			<div className="committee-component">
-				<div className="content-section left">
-					<div className="ornament image-ornament">
-						<img src={this.props.description.image} />
-					</div>
-				</div>
-				<div className="content-section right">
-					<div className="text-section">
-						<a target="_BLANK" href={this.props.description.link} key={this.props.description.name} className={`description ${this.props.description.class}`}>
-							<div className="info">
-								<h1><span>ACM</span> {this.props.description.name}</h1>
-							</div>
-						</a>
-						<p>{this.props.description.description}</p>
-					</div>
+				<img src={this.props.description.image} />
+				<div className="text-section">
+					<a target="_BLANK" href={this.props.description.link} key={this.props.description.name} className={`description ${this.props.description.class}`}>
+						<div className="info">
+							<h1><span>ACM</span> {this.props.description.name}</h1>
+						</div>
+					</a>
+					<p>{this.props.description.description}</p>
 				</div>
 			</div>
 		);
