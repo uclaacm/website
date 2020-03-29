654c44077778c0362941245363aa094cd463c020
William Chern <w.chern.9@gmail.com>
2020-03-28 22:09:07 -0700

    Update about page blurbs

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -21,9 +21,9 @@ export default class About extends React.Component {
 					</div>
 					<div className="text-section">
 						<h1>What is ACM?</h1>
-						<p className="half-size">ACM is the largest Computer Science student organization at UCLA and in Southern California. We welcome students of all different backgrounds and skill levels to join our community and share in our passion for CS!</p>
+						<p className="half-size">As a student chapter of the international <a href="https://www.acm.org" target="_BLANK">Association for Computing Machinery,</a> ACM at UCLA is the largest Computer Science student organization at UCLA and in Southern California. We welcome students of all different backgrounds and skill levels to join our community and share in our passion for CS!</p>
 						<p className="half-size">With events such as infosessions, workshops, speaker panels, hackathons, and competitions, we cover a variety of topics including artificial intelligence, virtual reality, cybersecurity, app development, diversity initiatives, community outreach, and much more.</p>
-						<p className="half-size">At ACM at UCLA, we aim to develop students’ technical and professional skills while cultivating an inclusive and supportive community for everyone interested in computer science, regardless of major or experience.</p>
+						<p className="half-size">At ACM, we aim to develop students’ technical and professional skills while cultivating an inclusive and supportive community for everyone interested in computer science, regardless of major or experience.</p>
 					</div>
 				</div>
 				<div className="content-section right">
@@ -42,7 +42,7 @@ export default class About extends React.Component {
 					<div className="text-section">
 						<h1>How do I get involved?</h1>
 						<p className="half-size">No matter what your background or major is, we welcome you to participate in all of our events and activities!</p>
-						<p className="half-size">To become a member and keep up with upcoming events, you should join <a href="https://members.uclaacm.com" target="_BLANK">the membership portal</a> and sign up for <a href="http://eepurl.com/c5pE6P" target="_BLANK">our weekly newsletter</a>. Signing up will keep you up to date with everything ACM, and earning points on the portal might even earn you a prize!</p>
+						<p className="half-size">To become a member and keep up with upcoming events, you should join <a href="https://members.uclaacm.com" target="_BLANK">our membership portal</a>, follow <a href="https://www.facebook.com/groups/uclaacm" target="_BLANK">our Facebook group</a>, and sign up for <a href="http://eepurl.com/c5pE6P" target="_BLANK">our weekly newsletter</a>. Signing up will keep you up to date with everything ACM, and earning points on the portal might even earn you a prize!</p>
 					</div>
 				</div>
 				<div className="content-section center" style={{marginBottom: '0px'}}>
