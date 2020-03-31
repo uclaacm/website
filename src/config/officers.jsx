910ab4583bb2b3976d7a468f1c90c5315dc053a0
William Chern <w.chern.9@gmail.com>
2020-03-30 21:23:53 -0700

    Update officers on About page for 2020-21

--- src/config/officers.jsx
+++ src/config/officers.jsx
@@ -1,74 +1,80 @@
 export default [
 	{
-		name: "Kevin Tan",
-		title: "President",
-		email: "ktan17@ucla.edu",
-		image: "/images/officers/KevinTan.jpg"
-	},
-	{
-		name: "Vivian Doan",
-		title: "External VP",
-		email: "viviandoan@engineering.ucla.edu",
-		image: "/images/officers/VivianDoan.jpg"
-	},
-	{
 		name: "Rucha Patki",
-		title: "Internal VP",
+		title: "President",
 		email: "ruchapatki@gmail.com",
 		image: "/images/officers/RuchaPatki.jpg"
 	},
 	{
-		name: "Megha Ilango",
-		title: "President of AI",
-		email: "meghailango@ucla.edu",
-		image: "/images/officers/MeghaIlango.jpg"
+		name: "Katherine Chong",
+		title: "External Vice President",
+		email: "kchong612@gmail.com",
+		image: "/images/officers/KatherineChong.jpg"
 	},
 	{
-		name: "Connie Chen",
-		title: "Co-President of Hack",
-		email: "conniechen@engineering.ucla.edu",
-		image: "/images/officers/ConnieChen.jpg"
+		name: "William Chern",
+		title: "Internal Vice President",
+		email: "chern@ucla.edu",
+		image: "/images/officers/WilliamChern.jpg"
 	},
 	{
-		name: "Lea Blum",
-		title: "Co-President of Hack",
-		email: "leab1308@gmail.com",
-		image: "/images/officers/LeaBlum.jpg"
+		name: "Raji Jadhav",
+		title: "President, Hack",
+		email: "rajeshwari.jadhav6@gmail.com",
+		image: "/images/officers/RajiJadhav.png"
 	},
 	{
-		name: "Justin Li",
-		title: "President of ICPC",
-		email: "justindli@gmail.com",
-		image: "/images/officers/JustinLi.jpg"
+		name: "Julia Baylon",
+		title: "President, ACM-W",
+		email: "juliabaylon01@gmail.com",
+		image: "/images/officers/JuliaBaylon.jpg"
+	},
+	{
+		name: "John Dang",
+		title: "President, AI",
+		email: "jamqd@ucla.edu",
+		image: "/images/officers/JohnDang.png"
+	},
+	{
+		name: "Kade Adams",
+		title: "Co-President, Game Studio",
+		email: "kadebadams@gmail.com",
+		image: "/images/officers/KadeAdams.jpg"
+	},
+	{
+		name: "Evan Charfauros",
+		title: "Co-President, Game Studio",
+		email: "evancharfauros@gmail.com",
+		image: "/images/officers/EvanCharfauros.jpg"
+	},
+	{
+		name: "Elena Escalas",
+		title: "President, ICPC",
+		email: "elenaescalas@g.ucla.edu",
+		image: "/images/officers/ElenaEscalas.jpg"
 	},
 	{
 		name: "Sanjana Sarda",
-		title: "President of Cyber",
+		title: "President, Cyber",
 		email: "sanjanas.sardas@gmail.com",
 		image: "/images/officers/SanjanaSarda.jpg"
 	},
 	{
-		name: "Kevin Chuang",
-		title: "President of Game Studio",
-		email: "kevinchuang2@gmail.com",
-		image: "/images/officers/KevinChuang.jpg"
+		name: "Matt Wang",
+		title: "President, Teach LA",
+		email: "matt@matthewwang.me",
+		image: "/images/officers/MattWang.jpg"
 	},
 	{
-		name: "Baolinh Nguyen",
-		title: "President of ACM-W",
-		email: "Baolinh.nguyen@ucla.edu",
-		image: "/images/officers/BaolinhNguyen.jpg"
+		name: "Haki Alpay",
+		title: "Co-President, Design",
+		email: "hak7alp@gmail.com",
+		image: "/images/officers/HakiAlpay.jpg"
 	},
 	{
-		name: "Isra Ameen",
-		title: "President of Design",
-		email: "israameen1@gmail.com",
-		image: "/images/officers/IsraAmeen.jpg"
-	},
-	{
-		name: "Christina I.",
-		title: "President of Teach LA",
-		email: "christina.indudhara@gmail.com",
-		image: "/images/officers/ChristinaIndudhara.jpg"
+		name: "Tomoki Fukazawa",
+		title: "Co-President, Design",
+		email: "tomoki@fukazawa.dev",
+		image: "/images/officers/TomokiFukazawa.png"
 	}
 ];
