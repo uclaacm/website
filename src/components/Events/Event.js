8523616e461312024e7f1ac80a7af58cc7a96347
vohndernet <22381311+vohndernet@users.noreply.github.com>
2020-09-12 23:48:39 -0700

    Removed COVID notice in Events page

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -11,7 +11,6 @@ export default class Events extends React.Component {
                <Banner decorative />
                <h2>Our Events</h2>
                <p>Our events are open to everyone, regardless of major, background, or experience!</p>
-               <b><p>Due to COVID-19, all ACM events during Spring 2020 will be online. Find us on YouTube <a href={Config.events.youtube_url}>here</a>!</p></b>
                <br/><br/>
                <Calendar url={Config.events.calendar_url}/>
                <br/><br/><br/>
