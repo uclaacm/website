94996bb082bc675851386f9c18cffb1162a35b2e
vohndernet <22381311+vohndernet@users.noreply.github.com>
2020-09-13 22:07:42 -0700

    Attached links to company logos in Sponsors page + logo updates
    Logos are clickable. Updated logos for Northrop-Grumman, Teradata, and Viasat.

--- src/config/sponsors.js
+++ src/config/sponsors.js
@@ -2,57 +2,71 @@ export default [
 	{
 		'name': 'American Express',
 		'image': '/images/sponsors/amex.svg',
+		'link': 'https://www.americanexpress.com/',
 	},
 	{
 		'name': 'Blizzard',
 		'image': '/images/sponsors/blizzard.png',
+		'link': 'https://www.blizzard.com/en-us/',
 	},
 	{
 		'name': 'Bloomberg',
 		'image': '/images/sponsors/bloomberg.svg',
+		'link': 'https://www.bloomberg.com/',
 	},
 	{
 		'name': 'Facebook',
 		'image': '/images/sponsors/facebook.svg',
+		'link': 'https://www.facebook.com/',
 	},
 	{
 		'name': 'Google',
 		'image': '/images/sponsors/google.svg',
+		'link': 'https://www.google.com/',
 	},
 	{
 		'name': 'KPMG',
 		'image': '/images/sponsors/kpmg.png',
+		'link': 'https://home.kpmg/us/en/home.html',
 	},
 	{
 		'name': 'Northrop Grumman',
-		'image': '/images/sponsors/northrop-grumman.png',
+		'image': '/images/sponsors/northrop-grumman.svg',
+		'link': 'https://www.northropgrumman.com/',
 	},
 	{
 		'name': 'Qualcomm',
 		'image': '/images/sponsors/qualcomm.svg',
+		'link': 'https://www.qualcomm.com/',
 	},
 	{
 		'name': 'Sandia National Laboratories',
 		'image': '/images/sponsors/snl.png',
+		'link': 'https://www.sandia.gov/',
 	},
 	{
 		'name': 'Strategic Global Advisors',
 		'image': '/images/sponsors/sgallc.jpg',
+		'link': 'https://sgadvisors.com/',
 	},
 	{
 		'name': 'Symantec',
 		'image': '/images/sponsors/symantec.jpg',
+		'link': 'https://www.broadcom.com/',
 	},
 	{
 		'name': 'Teradata',
-		'image': '/images/sponsors/teradata.jpg',
+		'image': '/images/sponsors/teradata.png',
+		'link': 'https://www.teradata.com/',
 	},
 	{
 		'name': 'Uber',
 		'image': '/images/sponsors/uber.png',
+		'link': 'https://www.uber.com/',
 	},
 	{
 		'name': 'ViaSat',
-		'image': '/images/sponsors/viasat.svg',
+		'image': '/images/sponsors/viasat.png',
+		'link': 'https://www.viasat.com/',
 	},
 ]
