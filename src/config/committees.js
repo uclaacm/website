df1b9b78322db999b75bb47861af02eb5eab9330
William Chern <w.chern.9@gmail.com>
2020-10-03 22:49:10 -0700

    Add diamond assets for future usage

--- src/config/committees.js
+++ src/config/committees.js
@@ -4,6 +4,7 @@ export default [
 		class: 'studio',
 		tagline: 'Creative Expression through Games and VR',
 		image: '/images/committees/studio_wordmark.svg',
+		diamond: '/images/diamonds/studio_diamond.svg',
 		link: 'https://www.facebook.com/groups/uclaacmstudio',
 	},
 	{
@@ -11,6 +12,7 @@ export default [
 		class: 'icpc',
 		tagline: 'Mastering Algorithms and Competitive Programming',
 		image: '/images/committees/icpc_wordmark.svg',
+		diamond: '/images/diamonds/icpc_diamond.svg',
 		link: 'https://www.facebook.com/groups/ucla.icpc',
 	},
 	{
@@ -18,6 +20,7 @@ export default [
 		class: 'design',
 		tagline: 'Form Follows Function',
 		image: '/images/committees/design_wordmark.svg',
+		diamond: '/images/diamonds/design_diamond.svg',
 		link: 'https://www.facebook.com/groups/acmdesign/'
 	},
 	{
@@ -25,6 +28,7 @@ export default [
 		class: 'cyber',
 		tagline: 'Cybersecurity Made Simple',
 		image: '/images/committees/cyber_wordmark.svg',
+		diamond: '/images/diamonds/cyber_diamond.svg',
 		link: 'https://acmcyber.com/',
 	},
 	{
@@ -32,6 +36,7 @@ export default [
 		class: 'teachla',
 		tagline: 'Learn It, Teach It',
 		image: '/images/committees/teachLA_wordmark.svg',
+		diamond: '/images/diamonds/teachLA_diamond.svg',
 		link: 'https://teachla.uclaacm.com'
 	},
 	{
@@ -39,6 +44,7 @@ export default [
 		class: 'w',
 		tagline: 'Starting Conversations about Diversity in Tech',
 		image: '/images/committees/w_wordmark.svg',
+		diamond: '/images/diamonds/w_diamond.svg',
 		link: 'https://www.facebook.com/groups/uclaacmw',
 	},
 	{
@@ -46,6 +52,7 @@ export default [
 		class: 'ai',
 		tagline: 'Learning Machine Learning',
 		image: '/images/committees/ai_wordmark.svg',
+		diamond: '/images/diamonds/ai_diamond.svg',
 		link: 'https://uclaacmai.github.io/',
 	},
 	{
@@ -53,6 +60,7 @@ export default [
 		class: 'hack',
 		tagline: 'Move Fast, Build Things',
 		image: '/images/committees/hack_wordmark.svg',
+		diamond: '/images/diamonds/hack_diamond.svg',
 		link: 'https://hack.uclaacm.com',
 	}
 ]
