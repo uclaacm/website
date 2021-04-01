8be674a4f8bea9cb24044352f105ef82c8e19ec3
Matt Wang <matt@matthewwang.me>
2021-03-31 18:29:21 -0700

    rm email, formatting

--- src/config/officers.js
+++ src/config/officers.js
@@ -2,85 +2,71 @@ export default [
 	{
 		name: "Rucha Patki",
 		title: "President",
-		email: "ruchapatki@gmail.com",
 		image: "/images/officers/RuchaPatki.jpg"
 	},
 	{
 		name: "Katherine Chong",
 		title: "External Vice President",
-		email: "kchong612@gmail.com",
 		image: "/images/officers/KatherineChong.jpg"
 	},
 	{
 		name: "William Chern",
 		title: "Internal Vice President",
-		email: "chern@ucla.edu",
 		image: "/images/officers/WilliamChern.jpg"
 	},
 	{
 		name: "Raji Jadhav",
 		title: "President, Hack",
-		email: "rajeshwari.jadhav6@gmail.com",
 		image: "/images/officers/RajiJadhav.jpg"
 	},
 	{
 		name: "Julia Baylon",
 		title: "President, ACM-W",
-		email: "juliabaylon01@gmail.com",
 		image: "/images/officers/JuliaBaylon.jpg"
 	},
 	{
 		name: "John Dang",
 		title: "President, AI",
-		email: "jamqd@ucla.edu",
 		image: "/images/officers/JohnDang.jpg"
 	},
 	{
 		name: "Kade Adams",
 		title: "Co-President, Game Studio",
-		email: "kadebadams@gmail.com",
 		image: "/images/officers/KadeAdams.jpg"
 	},
 	{
 		name: "Evan Charfauros",
 		title: "Co-President, Game Studio",
-		email: "evancharfauros@gmail.com",
 		image: "/images/officers/EvanCharfauros.jpg"
 	},
 	{
 		name: "Nikil Selvam",
 		title: "President, ICPC",
-		email: "nikilrselvam@gmail.com ",
 		image: "/images/officers/NikilSelvam.jpg"
 	},
-    {
-        name: "Disha Zambani",
-        title: "Co-President, Cyber",
-        email: "dmzambani@gmail.com",
-        image: "/images/officers/DishaZambani.jpg"
-    },
-    {
-        name: "Ellie Cheng",
-        title: "Co-President, Cyber",
-        email: "ellieyhc@gmail.com",
-        image: "/images/officers/EllieCheng.jpg"
-    },
+	{
+			name: "Disha Zambani",
+			title: "Co-President, Cyber",
+			image: "/images/officers/DishaZambani.jpg"
+	},
+	{
+			name: "Ellie Cheng",
+			title: "Co-President, Cyber",
+			image: "/images/officers/EllieCheng.jpg"
+	},
 	{
 		name: "Matt Wang",
 		title: "President, Teach LA",
-		email: "matt@matthewwang.me",
 		image: "/images/officers/MattWang.jpg"
 	},
 	{
 		name: "Haki Alpay",
 		title: "Co-President, Design",
-		email: "hak7alp@gmail.com",
 		image: "/images/officers/HakiAlpay.jpg"
 	},
 	{
 		name: "Tomoki Fukazawa",
 		title: "Co-President, Design",
-		email: "tomoki@fukazawa.dev",
 		image: "/images/officers/TomokiFukazawa.jpg"
 	}
 ];
