8ecaebccff977b17ee4335eac6ecbe5a375362b6
Matt Wang <matt@matthewwang.me>
2021-04-21 16:46:17 -0700

    fixes SocialMedia prop

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 function SocialMedia(props){
+    const iconType = props.type ? props.type : "";
 	return (
 		<React.Fragment>
-            <a href="https://www.facebook.com/uclaacm" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="facebook" /></a>
-            <a href="https://www.github.com/uclaacm" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="github" /></a>
-            <a href="https://www.instagram.com/acm.ucla/" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="instagram" /></a>
-            <a href="https://discord.gg/eWmzKsY" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="discord" /></a>
-            <a href="https://www.youtube.com/c/acmucla" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="youtube" style={{width: 35 + 'px'}} /></a>
-            <a href="https://medium.com/techatucla" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + props.type} id="medium" /></a>	
+            <a href="https://www.facebook.com/uclaacm" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="facebook" /></a>
+            <a href="https://www.github.com/uclaacm" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="github" /></a>
+            <a href="https://www.instagram.com/acm.ucla/" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="instagram" /></a>
+            <a href="https://discord.gg/eWmzKsY" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="discord" /></a>
+            <a href="https://www.youtube.com/c/acmucla" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="youtube" style={{width: 35 + 'px'}} /></a>
+            <a href="https://medium.com/techatucla" target="_blank" rel="noreferrer noopener"><div className={'sm-icon ' + iconType} id="medium" /></a>
         </React.Fragment>
 	);
 }
