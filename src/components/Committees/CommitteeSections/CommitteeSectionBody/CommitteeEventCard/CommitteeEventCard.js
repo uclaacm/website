197f6e858ee5823865a584f2ce7085a7afbf47c5
Charles Liu <liuchangyang112@gmail.com>
2021-05-04 17:24:57 +0800

    Revert CommitteeEventCard formatting to match master
    Revert automatic formatting by prettier

--- src/components/Committees/CommitteeSections/CommitteeSectionBody/CommitteeEventCard/CommitteeEventCard.js
+++ src/components/Committees/CommitteeSections/CommitteeSectionBody/CommitteeEventCard/CommitteeEventCard.js
@@ -1,21 +1,17 @@
-import React from "react";
+import React from 'react';
 
 function CommitteeEventCard(props) {
-  const hasImage = props.image.src;
-  return (
-    <div className="info-card-container">
-      {hasImage ? (
-        <img src={props.image.src} alt={props.image.alt} />
-      ) : (
-        <img
-          src={`/images/committees/${props.committee}/${props.committee}_motif_base.png`}
-          alt={props.image.alt}
-        />
-      )}
-      <h2>{props.title}</h2>
-      {props.desc}
-    </div>
-  );
+    const hasImage = props.image.src;
+    return (
+        <div className="info-card-container">
+            {hasImage
+                ? <img src={props.image.src} alt={props.image.alt} />
+                : <img src={`/images/committees/${props.committee}/${props.committee}_motif_base.png`} alt={props.image.alt} />
+            }
+            <h2>{props.title}</h2>
+            {props.desc}
+        </div>
+    );
 }
 
 export default CommitteeEventCard;
