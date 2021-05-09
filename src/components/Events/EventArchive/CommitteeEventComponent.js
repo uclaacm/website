e038d0c5f48da9e6a1cd86b592fe597e005689a0
dtjanaka <15240801+dtjanaka@users.noreply.github.com>
2021-05-08 19:49:05 -0700

    Beautify.

--- src/components/Events/EventArchive/CommitteeEventComponent.js
+++ src/components/Events/EventArchive/CommitteeEventComponent.js
@@ -8,32 +8,40 @@ function CommitteeEventComponent(props) {
   let ce = props.committeeEvent;
   let inPerson = ce.attendanceType === attendanceTypes.hybrid || ce.attendanceType === attendanceTypes.inperson;
   let virtual = ce.attendanceType === attendanceTypes.hybrid || ce.attendanceType === attendanceTypes.virtual;
+
+  function ceSession() {
+    if (ce.isTrack) {
+      return <h4 className='ce-session'>{'Session ' + ce.sessionNumber}</h4>;
+    }
+    return null;
+  }
   return (
     <div className='ce-container'>
       <div className='ce-title-container'>
-        <h2 className='ce-title'>{ce.eventName}</h2>
+        <div className='ce-title-container-first'>
+          <h3 className='ce-title'>{ce.eventName}</h3>
+          {ceSession()}
+          <h4 className='ce-committee'>
+            <span className='ce-committee-name' style={{backgroundColor: ce.committee.color, color: 'white'}}>{ce.committee.name}</span>
+          </h4>
+        </div>
         <EventTags tags={ce.tags}/>
       </div>
-      <hr/>
+      <hr className='ce-title-header-hr'/>
       <div className='ce-header'>
-        <h3 className='ce-committee'>
-          Hosted by
-          <span className='ce-committee-name' style={{backgroundColor: ce.committee.color, color: 'white'}}>{ce.committee.name}</span>
-        </h3>
-        <h3>{(ce.isTrack) ? 'Session ' + ce.sessionNumber : ''}</h3>
-        <h3>{ce.date.toLocaleDateString()}</h3>
-        <h3>{ce.quarter.season} {ce.quarter.year}</h3>
+        <h4>{ce.date.toLocaleDateString()}</h4>
+        <h4>{ce.quarter.season} {ce.quarter.year}</h4>
       </div>
       <br/>
       <div className='ce-more-info-container'>
         <div className='ce-event-type'>
-          <h3>{ce.attendanceType}
+          <h4>{ce.attendanceType}
               {(inPerson) ? ' · ' + ce.physicalLocation : ''}
               {(virtual) ? ' · ' : ''}
-              <a href={ce.virtualLocation}>{(virtual) ? 'Online location' : ''}</a>
-          </h3>
+              <a href={ce.virtualLocation}>{(virtual) ? 'Online' : ''}</a>
+          </h4>
         </div>
-        <h3>Taught by <i>{ce.teachers.join(', ')}</i></h3>
+        <h4>Taught by <i>{ce.teachers.join(', ')}</i></h4>
       </div>
       <br/>
       <div className='ce-links'>
