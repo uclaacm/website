1f1220d379bc509d11fd3c75e36a985ced16ce8b
changyang-liu <49157410+changyang-liu@users.noreply.github.com>
2021-05-11 11:35:06 +0800

    Manually change links on committees page to use <a> tag instead of anchorme (#135)
    * Change links in committees config to use <a> tags
    
    Wherever we used to use anchorme for the link, now change
    the string to use <p>, and manually make the link into
    an <a> tag.
    
    * Wrap emojis in span tag
    
    The linter will throw a warning unless you do this
    
    * Fix broken github link
    
    href of the link did not have https:// so it was not
    going to the page. Also change it to say "Github
    repository"
    
    * Remove anchorme dependency
    
    Remove it from the source code and package.json,
    since we aren't using it anymore
    
    * Add a line break in the AI for high school link
    
    The link was too long, so only 2 cards could fit in a row
    instead of 3
    
    * Revert "Change links in committees config to use <a> tags"
    
    This reverts commit 69a94aa7cb58bc902937e4135dd8508564ee9b43.
    
    * (Fixed) change links in committees config to use <a> tag
    
    Same changes as before, except now following the same formatting
    as master
    
    * Revert CommitteeEventCard formatting to match master
    
    Revert automatic formatting by prettier
    
    * Change whitespace strings to use single-quote
    
    As per eslintrc style rules
    
    * Update outdated committee event info
    
    One of the changes made in master were overwritten during the
    merge conflict
    
    * Make <p> paragraphs into one line
    
    Co-authored-by: Matt Wang <matt@matthewwang.me>

--- src/components/Committees/CommitteeSections/CommitteeSectionBody/CommitteeEventCard/CommitteeEventCard.js
+++ src/components/Committees/CommitteeSections/CommitteeSectionBody/CommitteeEventCard/CommitteeEventCard.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { Anchorme} from 'react-anchorme';
 
-function CommitteeEventCard(props){
-
-    const CustomLink = (props) => {
-        return (
-             <a {...props}>here</a>
-        );
-      };
-
+function CommitteeEventCard(props) {
     const hasImage = props.image.src;
-	return (
+    return (
         <div className="info-card-container">
             {hasImage
                 ? <img src={props.image.src} alt={props.image.alt} />
                 : <img src={`/images/committees/${props.committee}/${props.committee}_motif_base.png`} alt={props.image.alt} />
             }
             <h2>{props.title}</h2>
-            <Anchorme linkComponent={CustomLink} target="_blank" rel="noreferrer noopener">
-                {props.desc}
-            </Anchorme>
+            {props.desc}
         </div>
-	);
+    );
 }
 
 export default CommitteeEventCard;
--- src/config/committees.js
+++ src/config/committees.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default [
 	{
 		name: 'Studio',
@@ -22,7 +24,11 @@ export default [
 					src: '',
 					alt: '',
 				},
-				desc: 'Each year, Studio splits into several teams of students who each work throughout winter and spring quarter to put together a fully-fledged game, usually coding in C# with the Unity engine. You can see a collection of many past projects uclaacmstudio.itch.io.',
+				desc: (
+					<p>
+						Each year, Studio splits into several teams of students who each work throughout winter and spring quarter to put together a fully-fledged game, usually coding in C# with the Unity engine. You can see a collection of many past projects at <a href="https://uclaacmstudio.itch.io" target="_blank" rel="noopener noreferrer" >uclaacmstudio.itch.io</a>.
+					</p>
+				),
 			},
 			{
 				title: 'Beginner Unity track',
@@ -150,7 +156,11 @@ export default [
 					src: '/images/committees/design/styleguide.png',
 					alt: '',
 				},
-				desc: 'We are finishing the design styleguide which contains the new branding design assets necessary for all committees, and some do\'s and don\'ts with abiding by the new brand image. We are also extracting the styles used in the styleguide into a CSS framework that can be used by all ACM committees to create their sites in the ACM style. Check it out design.uclaacm.com and the Github github.com/uclaacm/Styleguide',
+				desc: (
+					<p>
+						We are finishing the design styleguide which contains the new branding design assets necessary for all committees, and some do&apos;s and don&apos;ts with abiding by the new brand image. We are also extracting the styles used in the styleguide into a CSS framework that can be used by all ACM committees to create their sites in the ACM style. Check it out at <a href="https://design.uclaacm.com" target="_blank" rel="noopener noreferrer">design.uclaacm.com</a> and the <a href="github.com/uclaacm/Styleguide" target="_blank" rel="noopener noreferrer" >Github page</a>.
+					</p>
+				),
 			},
 			{
 				title: 'UXLA: UI/UX Workshop Series',
@@ -281,7 +291,11 @@ export default [
 					src: '/images/committees/teachLA/passworks.png',
 					alt: 'Screenshot of Teach LA\'s passworks project',
 				},
-				desc: 'Write educational software to make teaching easier, either by developing interactive web applications called “Learning Labs” (e.g. Passworks Lesson passworks.uclaacm.com), maintain our free online code editor (editor.uclaacm.com), or bust out your design skills on our website teachla.uclaacm.com. No prior dev experience required!',
+				desc: (
+					<p>
+						Write educational software to make teaching easier, either by developing interactive web applications called “Learning Labs” (e.g. <a href="https://passworks.uclaacm.com" target="_blank" rel="noopener noreferrer">Passworks Lesson</a>), maintain our <a href="https://editor.uclaacm.com" target="_blank" rel="noopener noreferrer">free online code editor</a>, or bust out your design skills on <a href="https://teachla.uclaacm.com" target="_blank" rel="noopener noreferrer">our website</a>. No prior dev experience required!
+					</p>
+				),
 			},
 			{
 				title: 'Weekly Curriculum Meetings',
@@ -415,7 +429,11 @@ export default [
 					src: '',
 					alt: '',
 				},
-				desc: 'You Belong in AI! s a podcast on diversity in the field of Artificial Intelligence. We feature industry and academic leaders in AI from a diverse range of backgrounds to come and talk about their work and the importance of diversity and inclusion in AI. You Belong in AI! Is available on all major podcast streaming platforms anchor.fm/ucla-acm-ai',
+				desc: (
+					<p>
+						<a href="https://anchor.fm/ucla-acm-ai" target="_blank" rel="noopener noreferrer">You Belong in AI!</a> is a podcast on diversity in the field of Artificial Intelligence. We feature industry and academic leaders in AI from a diverse range of backgrounds to come and talk about their work and the importance of diversity and inclusion in AI. <a href="https://anchor.fm/ucla-acm-ai" target="_blank" rel="noopener noreferrer">You Belong in AI!</a> is available on all major podcast streaming platforms.
+					</p>
+				),
 			},
 			{
 				title: 'AI Course for High School Students (Collaboration with ACM Teach LA)',
@@ -423,7 +441,11 @@ export default [
 					src: '',
 					alt: '',
 				},
-				desc: 'ACM AI’s Outreach team collaborates with ACM TeachLA to teach an introductory AI course to local high school students in Los Angeles. An overview of this course content can be found teachla.uclaacm.com/classes/ml',
+				desc: (
+					<p>
+						ACM AI’s Outreach team collaborates with ACM TeachLA to teach an introductory AI course to local high school students in Los Angeles. An overview of this course content can be found at <a href="https://teachla.uclaacm.com/classes/ml" target="_blank" rel="noopener noreferrer">teachla.uclaacm.com/ classes/ml</a>.
+					</p>
+				),
 			},
 			{
 				title: 'AI/ML Workshops',
