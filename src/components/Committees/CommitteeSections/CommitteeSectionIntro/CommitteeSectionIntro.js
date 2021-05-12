eb180dd40724d03b2811e6171fdf7f6fd1b1f22b
Matt Wang <matt@matthewwang.me>
2021-05-12 16:39:56 -0700

    swaps all icons to Font Awesome

--- src/components/Committees/CommitteeSections/CommitteeSectionIntro/CommitteeSectionIntro.js
+++ src/components/Committees/CommitteeSections/CommitteeSectionIntro/CommitteeSectionIntro.js
@@ -1,4 +1,49 @@
+import {
+    faFacebook,
+    faInstagram,
+    faDiscord,
+    faTwitter,
+    faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+
+function CommitteeIconLink({committee, link}) {
+    const committeeStr = `acm ${committee.name}`;
+    const iconStr = link.platform === 'website' ? `${committeeStr}'s website` : `${committeeStr} on ${link.platform}`;
+    return (
+        <a
+            className={`icon-link ${committee.class}`}
+            href={link.link}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label={iconStr}
+        >
+            <CommitteeIcon platform={link.platform} />
+        </a>
+    );
+}
+
+function CommitteeIcon({platform}) {
+    // see https://fontawesome.com/how-to-use/on-the-web/other-topics/accessibility#web-fonts-semantic
+    // for more on accessibility
+    switch(platform) {
+        case 'discord':
+            return <FontAwesomeIcon icon={faDiscord} fixedWidth aria-hidden={true} />;
+        case 'facebook':
+            return <FontAwesomeIcon icon={faFacebook} fixedWidth aria-hidden={true} />;
+        case 'github':
+            return <FontAwesomeIcon icon={faGithub} fixedWidth aria-hidden={true} />;
+        case 'instagram':
+            return <FontAwesomeIcon icon={faInstagram} fixedWidth aria-hidden={true} />;
+        case 'twitter':
+            return <FontAwesomeIcon icon={faTwitter} fixedWidth aria-hidden={true} />;
+        case 'website':
+        default:
+            return <FontAwesomeIcon icon={faLink} fixedWidth aria-hidden={true} />;
+    }
+}
 
 function CommitteeSectionIntro(props) {
     return (
@@ -12,7 +57,9 @@ function CommitteeSectionIntro(props) {
                 <p>{props.committee.mission}</p>
                 <div className="learn-more">
                     <h3>Learn More</h3>
-                    { props.committee.links.map(link => <a href={link.link} target="_blank" rel="noreferrer noopener" key={link.platform}><div className={'icon ' + props.committee.class} id={link.platform} /></a>) }
+                    {props.committee.links.map(link =>
+                        <CommitteeIconLink committee={props.committee} link={link} key={link.platform} />,
+                    )}
                 </div>
             </div>
             <div className="right-col">
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ function Footer(){
 					</div>
 					<div className="footer-item">
 						<h3>Reach us at</h3>
-						<a href="mailto: acm@ucla.edu" className="email"><span>acm@ucla.edu</span></a>
+						<a href="mailto: acm@ucla.edu" className="email"><span className="footer-text">acm@ucla.edu</span></a>
 					</div>
 					{/* the paddingTop is a magic number that replaces a
 						previous empty <h3> tag previously used for spacing. should be replaced later.
@@ -30,7 +30,7 @@ function Footer(){
 					</a>
 				</div>
 				<div id="footer-bottom">
-					<span>&copy; ACM at UCLA 2021.</span>
+					<span className="footer-text">&copy; ACM at UCLA 2021.</span>
 				</div>
 			</div>
 		</div>
