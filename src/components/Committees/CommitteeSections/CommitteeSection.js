ae922609053dab4fc470b282094388541effa4ff
Matt Wang <matt@matthewwang.me>
2021-06-11 14:28:49 -0700

    first pass on flattening

--- src/components/Committees/CommitteeSections/CommitteeSection.js
+++ src/components/Committees/CommitteeSections/CommitteeSection.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import Body from './CommitteeSectionBody/CommitteeSectionBody';
-import Header from './CommitteeSectionHeader/CommitteeSectionHeader';
+import CommitteeEventCard from './CommitteeEventCard/CommitteeEventCard';
 import Intro from './CommitteeSectionIntro/CommitteeSectionIntro';
 
-function CommitteeSection(props) {
+function CommitteeProjects({committee}) {
 	return (
-		<div id={props.committee.class} className='committee-section'>
-			<Header committee={props.committee}/>
-			<Intro committee={props.committee}/>
-			<Body committee={props.committee}/>
+		<div className="body-wrapper">
+			<h3 className="font-header text-bold text-25x">Events and Projects</h3>
+			<div className="card-container">
+				{committee.infoCards.map((card) => {
+					// TODO: destructure class after renaming it
+					const { image, title, desc } = card;
+					return (
+						<CommitteeEventCard
+							committee={committee.class}
+							image={image}
+							title={title}
+							desc={desc}
+							key={title}
+						/>
+					);
+				})}
+				</div>
 		</div>
 	);
+}
+
+function CommitteeSection(props) {
+  const { committee } = props;
+  // TODO: destructure committee after renaming the .class property
+  return (
+    <div id={committee.class} className="committee-section">
+      {/* Header image */}
+			{/* TODO: this could probably be a bit more accessible */}
+      <div className={`committee-header ${committee.class}`}>
+        <img src={committee.image} alt="committee logo" />
+      </div>
+      {/* Committee Intro */}
+      <Intro committee={committee} />
+      {/* Committee Body: events & projects */}
+      <CommitteeProjects committee={committee} />
+    </div>
+  );
 }
 
 export default CommitteeSection;
--- src/components/Committees/Committees.js
+++ src/components/Committees/Committees.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Config from '../../config';
 import Banner from '../Banner/Banner';
-import CommitteeBanner from './CommitteesBanner/CommitteesBanner';
-import CommitteeSections from './CommitteeSections/CommitteeSections';
-import Navigation from './SidebarItem/SidebarItem';
+import CommitteeSection from './CommitteeSections/CommitteeSection';
+import Navigation from './Sidebar/Sidebar';
+
+function CommitteesBanner() {
+	return (
+		<div className="committee-banner-container">
+			<div className="banner-main-content">
+				<img src={process.env.PUBLIC_URL + '/images/acm_committees.png'} alt="the logos of all ACM committees" />
+				<div className="info">
+					<h1>Committees</h1>
+					<p className="half-size">ACM comprises eight committees, each serving a unique topic and mission. All of our events are open to everyone. We strive to cover all of our members’ interests and encourage members to explore new topics and events, too!</p>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 function Sponsors() {
+	const committees = Config.committees;
 	return (
 		<div>
 			<Banner decorative />
-			<Navigation committees={Config.committees} />
+			<Navigation committees={committees} />
 			<div className="page-content">
-			<CommitteeBanner />
-			<CommitteeSections committees={Config.committees}/>
+				<CommitteesBanner />
+				<div className='committee-sections-container'>
+					{committees.map(committee => <CommitteeSection key={committee.name} committee={committee} />) }
+				</div>
 			</div>
 		</div>
 	);
