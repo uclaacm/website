0856d9cf6151b9953d19777fa4729f2362ca726b
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-06-20 21:47:00 +0530

    fixed committee navbar image sizing

--- components/Committees/Sidebar.js
+++ components/Committees/Sidebar.js
@@ -7,12 +7,13 @@ function SidebarLink(props){
 			className={`committee ${props.committee.class}`}
 			href={`#${props.committee.class}`}
 		>
-			<Image
-				src={process.env.PUBLIC_URL + props.committee.image}
-				alt={`Logo and Wordmark for ACM ${props.committee.name}`}
-				width={200}
-				height={63}
-			/>
+ 			<div className="committee-sidebar-image">
+				<Image src={process.env.PUBLIC_URL + props.committee.image} 
+					layout="fill"
+					objectFit="contain"
+					objectPosition="left"
+					alt={`Logo and Wordmark for ACM ${props.committee.name}`} />
+			</div>
 		</a>
 	);
 }
