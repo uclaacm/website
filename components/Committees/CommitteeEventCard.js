68fff49f0ae3b8babc8aac738bde9b6bb529384d
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-06-24 08:34:14 +0530

    fixed image border issues on committee pages
    (combination of 3 commits - messages below)
    
    added borders to committee page images
    also added margin before intro section for gap after committee logo
    issue currently - 'padding' between image and it's surrounding div causing the border to be slightly off
    
    fixed border issue for committees images
    also added comment explaining rationale behind gnarly css
    
    ran linter

--- components/Committees/CommitteeEventCard.js
+++ components/Committees/CommitteeEventCard.js
@@ -5,10 +5,12 @@ function CommitteeEventCard(props) {
     const hasImage = props.image.src;
     return (
         <div className="info-card-container">
+            <div className="image-wrapper">
             {hasImage
                 ? <Image src={props.image.src} alt={props.image.alt} width={241.8} height={160}/>
                 : <Image src={`/images/committees/${props.committee}/${props.committee}_motif_base.png`} alt={props.image.alt} width={241.8} height={160} />
             }
+            </div>
             <h4 className="font-header text-bold">{props.title}</h4>
             {props.desc}
         </div>
--- components/Committees/CommitteeSectionIntro.js
+++ components/Committees/CommitteeSectionIntro.js
@@ -53,7 +53,9 @@ function CommitteeSectionIntro(props) {
         <div className="grid-tablet-2">
             <div className="grid-tablet-2-reverse">
                 {/* TODO: we may want to make this image layout="fill"? it seems like pre-next, the proportion of the image changes, which is very tricky to deal with */}
-                <Image src={props.committee.introImage} alt={`large motif image for ${props.committee.name}`} width={390} height={195}/>
+                <div className="image-wrapper">
+                    <Image src={props.committee.introImage} alt={`large motif image for ${props.committee.name}`} width={390} height={195}/>
+                </div>
             </div>
             <div className="pr-tablet-2">
                 <h2>{props.committee.tagline}</h2>
