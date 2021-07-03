d94a8e17ee5f1767a2b6f43a7ad11094f13856cc
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-07-03 17:33:08 +0530

    added timeline dependency and rough timeline

--- pages/internship.js
+++ pages/internship.js
@@ -2,9 +2,18 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Layout from '../components/Layout';
 import data from '../data';
+import { Chrono } from "react-chrono";
+
 
 function InternshipPage(){
     const { committees } = data;
+    const items = [
+        {title: '00/00 Apps open',},
+        {title: "00/00 Apps due",},
+        {title: "00/00 Interviews",},
+        {title: "00/00 Hear back",},
+        {title: "00/00 Onboarding",},
+    ];
     return(
         <Layout>
             <Banner decorative />
@@ -47,6 +56,14 @@ function InternshipPage(){
             </div>
             <div className="content-section text-center">
                 <h1>Applications</h1>
+                <div style={{ width: "100%", height: "400px" }}>
+                    <Chrono 
+                        items={items}  
+                        hideControls
+                        itemWidth={150}
+                        theme={{primary: "#1E6CFF", secondary: "#181818"}}
+                        />
+                </div>
             </div>
             <div className="content-section text-center">
                 <h1>Next steps</h1>
