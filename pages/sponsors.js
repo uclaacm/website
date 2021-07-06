37f07ffba383557cd7cb0cba3fbe3097b1feaa53
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-07-06 16:47:34 +0530

    converted sponsors page to modular scss TODO: change import when button component is converted

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -9,30 +9,32 @@ import data from '../data';
 import reslifeLogo from '../public/images/sponsors/reslife.png';
 import uclaCsDeptLogo from '../public/images/sponsors/ucla_cs_dept.png';
 
+import styles from "../styles/pages/Sponsors.module.scss";
+
 function Sponsors() {
 	const { sponsors } = data;
 	return (
 		<Layout>
 			<Banner decorative />
-			<div className="content-section text-center">
+			<div className={`${styles.contentSection} ${styles.textCenter}`}>
 				<h1>Our Sponsors</h1>
-				<p className="subheading">A big thank-you to the <a href="https://www.cs.ucla.edu" target="_blank" rel="noreferrer noopener">UCLA Computer Science Department</a> for their continuous partnership with and support of ACM at UCLA. Shout-out to <a href="https://reslife.ucla.edu" target="_blank" rel="noreferrer noopener">UCLA ResLife</a> for providing logistical assistance and stellar venues for our events.</p>
-				<div className="sponsor-container">
-					<div className="sponsor">
+				<p className={styles.subheading}>A big thank-you to the <a href="https://www.cs.ucla.edu" target="_blank" rel="noreferrer noopener">UCLA Computer Science Department</a> for their continuous partnership with and support of ACM at UCLA. Shout-out to <a href="https://reslife.ucla.edu" target="_blank" rel="noreferrer noopener">UCLA ResLife</a> for providing logistical assistance and stellar venues for our events.</p>
+				<div className={styles.sponsorContainer}>
+					<div className={styles.sponsor}>
 						<a href="https://www.cs.ucla.edu" target="_blank" rel="noreferrer noopener">
 							<Image src={uclaCsDeptLogo} alt="UCLA CS Dept Logo" priority={true} />
 						</a>
 					</div>
-					<div className="sponsor">
+					<div className={styles.sponsor}>
 						<a href="https://reslife.ucla.edu" target="_blank" rel="noreferrer noopener">
 							<Image src={reslifeLogo} alt="UCLA ResLife Logo" priority={true} />
 						</a>
 					</div>
 				</div>
-				<p className="subheading">A special thanks to all of our UCLA Computer Science industry affiliates for their support:</p>
-				<div className="sponsor-container">
+				<p className={styles.subheading}>A special thanks to all of our UCLA Computer Science industry affiliates for their support:</p>
+				<div className={styles.sponsorContainer}>
 					{sponsors.map(sponsor =>
-						<a className="block-centered sponsor" href={sponsor.link} target="_blank" rel="noreferrer noopener" key={sponsor.name}>
+						<a className={`${styles.blockCentered} ${styles.sponsor}`} href={sponsor.link} target="_blank" rel="noreferrer noopener" key={sponsor.name}>
 							<Image
 								alt={sponsor.name}
 								src={sponsor.image}
@@ -41,9 +43,9 @@ function Sponsors() {
 					)}
 				</div>
 				<h2>Interested in becoming a sponsor?</h2>
-				<div className="button-section">
+				<div className={styles.buttonSection}>
 					<Link href="mailto:acm@ucla.edu">
-						<a className="button">
+						<a className={styles.button}>
 							Contact Us at acm@ucla.edu
 						</a>
 					</Link>
