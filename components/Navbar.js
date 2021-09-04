e1594c06326d31e1461c500ed135769ded23fc15
Tanaya Nawathe <tnawathe@ucla.edu>
2021-09-04 10:16:52 -0700

    moved navbar changes to new branch

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,29 +7,21 @@ import React from 'react';
 //uncomment the above import line "import gmData from '../../config/gmData';" to get quarter from the gmData page
 //uncomment the two lines saying  "/* <Link href="/gm"><li>{gmData.date.quarter} gm</li></Link> */"
 
-function MobileNavItem(props) {
-	if (props.committees) {
-		return (
-				<ul className="committee-mobile-nav">
-					<li><Link href={'/committees#studio'}><a>Studio</a></Link></li>
-					<li><Link href={'/committees#icpc'}><a>ICPC</a></Link></li>
-					<li><Link href={'/committees#design'}><a>Design</a></Link></li>
-					<li><Link href={'/committees#cyber'}><a>Cyber</a></Link></li>
-					<li><Link href={'/committees#teachla'}><a>Teach LA</a></Link></li>
-					<li><Link href={'/committees#w'}><a>W</a></Link></li>
-					<li><Link href={'/committees#ai'}><a>AI</a></Link></li>
-					<li><Link href={'/committees#hack'}><a>Hack</a></Link></li>
-				</ul>
-		);
-	}
-	else {
-		return (
+function MobileNavItem() {
+	return (
+		<li id="committee-mobile-nav-item">
 			<ul className="committee-mobile-nav">
-					<li><Link href={'/impact'}><a>Impact</a></Link></li>
-					<li><Link href={''}><a>Jedi</a></Link></li>
+				<li><Link href={'/committees#studio'}><a>Studio</a></Link></li>
+				<li><Link href={'/committees#icpc'}><a>ICPC</a></Link></li>
+				<li><Link href={'/committees#design'}><a>Design</a></Link></li>
+				<li><Link href={'/committees#cyber'}><a>Cyber</a></Link></li>
+				<li><Link href={'/committees#teachla'}><a>Teach LA</a></Link></li>
+				<li><Link href={'/committees#w'}><a>W</a></Link></li>
+				<li><Link href={'/committees#ai'}><a>AI</a></Link></li>
+				<li><Link href={'/committees#hack'}><a>Hack</a></Link></li>
 			</ul>
-		);
-	}
+		</li>
+	);
 }
 
 
@@ -65,7 +57,7 @@ export default class Navbar extends React.Component {
 
 	render() {
 		return (
-			<div id="navbar">
+			<div id="navbar" role="banner">
 				<div id="navbar-inner">
 					<Link href="/">
 						<a id="nav-title" className="nav-section left">
@@ -75,19 +67,20 @@ export default class Navbar extends React.Component {
 							{/* TODO: use next image without breaking deploy */}
 						</a>
 					</Link>
-					<div className="nav-section right" id="desktop-nav">
+					<div className="nav-section right" id="desktop-nav" role="navigation">
 						<ul className="nav-items">
 							<li><Link href="/about"><a>About</a></Link></li>
 							<li><Link href="/committees"><a>Committees</a></Link></li>
 							{/* <li><Link href="/gm"><a>{gmData.date.quarter} GM</a></Link></li> */}
 							<li><Link href="/events"><a>Events</a></Link></li>
-							<li className="initiatives"><Link href="/impact"><a>Initiatives</a></Link></li>
 							<li><Link href="/sponsors"><a>Sponsors</a></Link></li>
 							<li><Link href="https://members.uclaacm.com"><a className="button button-transparent button-lg font-header">Member Login</a></Link>
 							</li>
 						</ul>
 					</div>
 					<div className="nav-section right" id="mobile-nav">
+						{/* TODO: resolve this by refactoring the navbar */}
+						{/* eslint-disable-next-line jsx-a11y/label-has-for */}
 						<label htmlFor="menu-toggle">
 							<div className="hamburger-icon">
 								<div className="bar" id="top-bar" />
@@ -96,15 +89,13 @@ export default class Navbar extends React.Component {
 							</div>
 						</label>
 						<input type="checkbox" id="menu-toggle" />
-						<div id="hamburger-menu">
+						<div id="hamburger-menu" role="navigation">
 							<ul className="nav-items">
 								<li><Link href="/about"><a>About</a></Link></li>
 								<li><Link href="/committees"><a>Committees</a></Link></li>
 								{/* <li><Link href="/gm"><a>{gmData.date.quarter} GM</a></Link></li> */}
-								<MobileNavItem committees/>
+								<MobileNavItem/>
 								<li><Link href="/events"><a>Events</a></Link></li>
-								<li><Link href="/impact"><a>Initiatives</a></Link></li>
-								<MobileNavItem initiatives/>
 								<li><Link href="/sponsors"><a>Sponsors</a></Link></li>
 								<li><Link href="https://members.uclaacm.com"><a className="button button-transparent button-lg font-header">Member Login</a></Link>
 								</li>
