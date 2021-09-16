9ce93b10f18f1467510df022a296c0bbb237d859
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-09-16 11:22:02 -0700

    replaces b and br with h4

--- data/internship.js
+++ data/internship.js
@@ -43,14 +43,14 @@ export default {
         {
             name: 'Maya Raman',
             quote: <>
-                <b>What is your favorite memory from being an intern?</b><br/>
+                <h4>What is your favorite memory from being an intern?</h4>
                 Accidentally breaking tables at the intern retreat
-                <br/><br/><b>How much CS experience did you have before you became an intern?</b><br/>
+                <h4>How much CS experience did you have before you became an intern?</h4>
                 Not much - I took AP CS in high school and didn’t learn a ton,
                 and when I applied to be an intern I was taking CS 31.
-                <br/><br/><b>Are you still an officer in ACM? If so, in what Committee?</b><br/>
+                <h4>Are you still an officer in ACM? If so, in what Committee?</h4>
                 Yes! I’m the director of ACM Impact!
-                <br/><br/><b>What advice would you give to prospective interns?</b><br/>
+                <h4>What advice would you give to prospective interns?</h4>
                 Try to maximize the amount of rejections you get - try everything!
             </>,
             image: '.',
@@ -60,15 +60,15 @@ export default {
         {
             name: 'Archisha Datta',
             quote: <>
-                <b>What is your favorite memory from being an intern?</b><br/>
+                <h4>What is your favorite memory from being an intern?</h4>
                 My favorite memory is the first meeting we had for the learning lab I was working on.
                 At the time, it seemed really scary to be thrown into a project with not a lot of experience,
                 but looking back on it I’m proud of how much I learned since then.
-                <br/><br/><b>How much CS experience did you have before you became an intern?</b><br/>
+                <h4>How much CS experience did you have before you became an intern?</h4>
                 I only knew basic programming (CS31) level when I became an intern.
-                <br/><br/><b>Are you still an officer in ACM? If so, in what Committee?</b><br/>
+                <h4>Are you still an officer in ACM? If so, in what Committee?</h4>
                 Yes, I’m the design director for TeachLA and I’m also getting involved in Design starting fall!
-                <br/><br/><b>What advice would you give to prospective interns?</b><br/>
+                <h4>What advice would you give to prospective interns?</h4>
                 If you’re on the fence about applying because you think you don’t have enough experience or
                 you don’t feel like you belong in CS culture, I would give it a shot anyways!
                 You might surprise yourself.
