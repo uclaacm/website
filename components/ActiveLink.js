abed8f5294c9331fb833af5cde98511f20944281
Vincent Geesman <vgeesman@gmail.com>
2021-09-23 21:04:37 -0700

    Reimplement navbar as per #246 (#253)
    * switched from fixed position menu to flexbox layout. should now be responsive to more screen sizes.
    * animation for hamburger seemed broken so I used on of my favorites. simply visual and can be changed as desired.
    * removed the checkbox implementation of hamburger menu and replaced with state to apply active class.
    * there were colors overwriting each other I used the ones tagged important but kept overwrites as comments. not sure which was intentional.
    * fixed an issue with the entire nested ul highlighting at once.
    * only one menu markup is used. on page break, css reshapes it to fit the modal.
    * noticed on smaller screens that modal links don't fit height. added scrolling to try and address this issue.

---
+++ components/ActiveLink.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { Children } from 'react';
+
+const ActiveLink = ({ children, activeClassName, ...props }) => {
+  const { asPath } = useRouter();
+  const child = Children.only(children);
+  const childClassName = child.props.className || '';
+
+  // if path equals href append activeClassName to childClassName otherwise return childClassName
+  const className = asPath === props.href || asPath === props.as
+    ? `${childClassName} ${activeClassName}`.trim()
+    : childClassName;
+
+    return (
+      //clones child with className if className exists
+      <Link {...props}>
+        {React.cloneElement(child, {
+          className: className || null,
+        })}
+      </Link>
+    );
+};
+
+export default ActiveLink;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,109 +1,58 @@
-// import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-// import gmData from '../../config/gmData';
+import React, { useState } from 'react';
+import ActiveLink from './ActiveLink';
 
-//to allow GM page to be visible on the navbar and accessible
-//uncomment the above import line "import gmData from '../../config/gmData';" to get quarter from the gmData page
-//uncomment the two lines saying  "/* <Link href="/gm"><li>{gmData.date.quarter} gm</li></Link> */"
+export default function Navbar() {
+  // set states
+  const [menuActive, setMenuActive] = useState(false);
 
-function MobileNavItem() {
-	return (
-		<li id="committee-mobile-nav-item">
-			<ul className="committee-mobile-nav">
-				<li><Link href={'/committees#studio'}><a>Studio</a></Link></li>
-				<li><Link href={'/committees#icpc'}><a>ICPC</a></Link></li>
-				<li><Link href={'/committees#design'}><a>Design</a></Link></li>
-				<li><Link href={'/committees#cyber'}><a>Cyber</a></Link></li>
-				<li><Link href={'/committees#teachla'}><a>Teach LA</a></Link></li>
-				<li><Link href={'/committees#w'}><a>W</a></Link></li>
-				<li><Link href={'/committees#ai'}><a>AI</a></Link></li>
-				<li><Link href={'/committees#hack'}><a>Hack</a></Link></li>
-			</ul>
-		</li>
-	);
-}
+  // switches mobile menu state
+  const menuActivate = () => {
+    setMenuActive(menuActive ? false : true);
+  };
 
-
-export default class Navbar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.clickMobileNav = this.clickMobileNav.bind(this);
-	}
-
-	clickMobileNav() {
-		// TODO: remove direct DOM manipulation, make this stateful
-		document.getElementById('menu-toggle').checked = false;
-	}
-
-	// TODO: can we both refactor this to use useEffect, and/or remove
-	// this listener entirely
-
-	componentDidMount() {
-		// TODO: remove direct DOM manipulation, make this stateful
-		const items = document.querySelectorAll('#mobile-nav .nav-items li');
-		for (const item of items) {
-			item.addEventListener('click', this.clickMobileNav);
-		}
-	}
-
-	componentWillUnmount() {
-		// TODO: remove direct DOM manipulation, make this stateful
-		const items = document.querySelectorAll('#mobile-nav .nav-items li');
-		for (const item of items) {
-			item.removeEventListener('click', this.clickMobileNav);
-		}
-	}
-
-	render() {
-		return (
-			<div id="navbar" role="banner">
-				<div id="navbar-inner">
-					<Link href="/">
-						<a id="nav-title" className="nav-section left">
-							{/* TODO: resolve next/image issue */}
-							{/* eslint-disable-next-line @next/next/no-img-element */}
-							<img src={'/images/acm_wordmark&logo.svg'} id="acm-logo" alt="ACM at UCLA Logo"></img>
-							{/* TODO: use next image without breaking deploy */}
-						</a>
-					</Link>
-					<div className="nav-section right" id="desktop-nav" role="navigation">
-						<ul className="nav-items">
-							<li><Link href="/about"><a>About</a></Link></li>
-							<li><Link href="/committees"><a>Committees</a></Link></li>
-							{/* <li><Link href="/gm"><a>{gmData.date.quarter} GM</a></Link></li> */}
-							<li><Link href="/events"><a>Events</a></Link></li>
-							<li><Link href="/sponsors"><a>Sponsors</a></Link></li>
-							<li><Link href="https://members.uclaacm.com"><a className="button button-transparent button-lg font-header">Member Login</a></Link>
-							</li>
-						</ul>
-					</div>
-					<div className="nav-section right" id="mobile-nav">
-						{/* TODO: resolve this by refactoring the navbar */}
-						{/* eslint-disable-next-line jsx-a11y/label-has-for */}
-						<label htmlFor="menu-toggle">
-							<div className="hamburger-icon">
-								<div className="bar" id="top-bar" />
-								<div className="bar" id="middle-bar" />
-								<div className="bar" id="bottom-bar" />
-							</div>
-						</label>
-						<input type="checkbox" id="menu-toggle" />
-						<div id="hamburger-menu" role="navigation">
-							<ul className="nav-items">
-								<li><Link href="/about"><a>About</a></Link></li>
-								<li><Link href="/committees"><a>Committees</a></Link></li>
-								{/* <li><Link href="/gm"><a>{gmData.date.quarter} GM</a></Link></li> */}
-								<MobileNavItem/>
-								<li><Link href="/events"><a>Events</a></Link></li>
-								<li><Link href="/sponsors"><a>Sponsors</a></Link></li>
-								<li><Link href="https://members.uclaacm.com"><a className="button button-transparent button-lg font-header">Member Login</a></Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  return(
+    <nav id="navbar">
+      <section id="nav-container">
+        <Link href="/">
+          <a id="nav-title" className="nav-section left" aria-label="acm home">
+            {/* TODO: resolve next/image issue */}
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={'/images/acm_wordmark&logo.svg'} id="acm-logo" alt="ACM at UCLA Logo"></img>
+            {/* TODO: use next image without breaking deploy */}
+          </a>
+        </Link>
+        <section id="nav-items-container">
+          <button className={menuActive ? 'active' : ''} id="hamburger" type="button" onClick={menuActivate} aria-label="navigation menu" aria-expanded={menuActive} tabIndex="0">
+            <span className="bar" id="bar-one"></span>
+            <span className="bar" id="bar-two"></span>
+            <span className="bar" id="bar-three"></span>
+          </button>
+          <section className={menuActive ? 'active' : ''} id="menu-modal">
+            <ul className={`nav-items ${menuActive ? 'active' : ''}`} role="presentation">
+              <li><ActiveLink activeClassName="active" href="/about" passHref={true}><button type="button" role="link" onClick={menuActivate}>About</button></ActiveLink></li>
+              <li><ActiveLink activeClassName="active" href="/committees" passHref={true}><button type="button" role="link" onClick={menuActivate}>Committees</button></ActiveLink></li>
+              {/* <li><Link href="/gm"><a>{gmData.date.quarter} GM</a></Link></li> */}
+              <li id="committee-mobile-nav-item">
+                <ul className="committee-mobile-nav" role="presentation">
+                  <li><ActiveLink activeClassName="active" href="/committees#studio" passHref={true}><button type="button" onClick={menuActivate}>Studio</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#icpc" passHref={true}><button type="button" onClick={menuActivate}>ICPC</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#design" passHref={true}><button type="button" onClick={menuActivate}>Design</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#cyber" passHref={true}><button type="button" onClick={menuActivate}>Cyber</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#teachla" passHref={true}><button type="button" onClick={menuActivate}>Teach LA</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#w" passHref={true}><button type="button" onClick={menuActivate}>W</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#ai" passHref={true}><button type="button" onClick={menuActivate}>AI</button></ActiveLink></li>
+                  <li><ActiveLink activeClassName="active" href="/committees#hack" passHref={true}><button type="button" onClick={menuActivate}>Hack</button></ActiveLink></li>
+                </ul>
+              </li>
+              <li><ActiveLink activeClassName="active" href="/events" passHref={true}><button type="button" role="link" onClick={menuActivate}>Events</button></ActiveLink></li>
+              <li><ActiveLink activeClassName="active" href="/sponsors" passHref={true}><button type="button" role="link" onClick={menuActivate}>Sponsors</button></ActiveLink></li>
+              <li><Link href="https://members.uclaacm.com"><a className="button button-transparent button-lg font-header">Member Login</a></Link>
+              </li>
+            </ul>
+          </section>
+        </section>
+      </section>
+    </nav>
+  );
 }
