91e04333bd97402c5f854f33f45fcc442267b0b3
evanzhong <evanzhong@ucla.edu>
2021-09-26 19:33:52 -0700

    flippable cards

--- components/Internship/NextSteps.js
+++ components/Internship/NextSteps.js
@@ -5,7 +5,7 @@ import styles from '../../styles/components/Internship/NextSteps.module.scss';
 
 function NextSteps(props) {
     const { image, name, info} = props;
-    const [isFlipped, setFlipped] = useState(true);
+    const [isFlipped, setFlipped] = useState(false);
     return (
         <button className={styles['committee-card']} onClick={() => setFlipped(!isFlipped)} role='tab'>
             <div className={`${styles['next-steps-card-inner']} ${styles[isFlipped ? null : 'is-flipped']}`}>
@@ -16,7 +16,7 @@ function NextSteps(props) {
                         layout='fill'
                     />
                 </div>
-                <div className={`${styles['next-steps-card-face']} ${styles.back}`}>
+                <div className={`${styles['next-steps-card-face']}`}>
                     <h3 className={styles['card-info-name']}>{name}</h3>
                     <p>{info}</p>
                 </div>
