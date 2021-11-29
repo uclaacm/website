b07aeb6ddd5302765b396bfd011a56f68f75070f
Ramsey <ramseyboyce@gmail.com>
2021-11-29 01:54:28 -0800

    added countdown timer component

---
+++ components/CountdownElement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Countdown from 'react-countdown';
+
+export default function CountdownElement(props) {
+    const chooseWhatToRender = ({ completed }) => {
+        if (completed) { 
+            return <>{props.children}</>; 
+        }
+        else {
+            return <>{props.fallback}</>;
+        }
+    };
+
+    return (
+        <Countdown date={props.switchTime} renderer={chooseWhatToRender} />
+	);
+}
