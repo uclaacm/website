fb57572b42c7adac70163f92fc3c08a03e4c7507
advaithg <gowrishettyadvaith@gmail.com>
2022-01-06 19:53:01 -0800

    changes banner text

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ function Home () {
 			/>
 			<div className="home-page text-center">
 				<ContentBanner
-					title="Check out our Winter General Meeting!"
+					title="See what happened at our Winter General Meeting!"
 					ctaText="Learn More"
 					ctaLink="/gm/w22"
 				/>
