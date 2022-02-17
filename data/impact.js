81170ed66187abd0ce5eb415404cf42762c48480
spatlol <siddharth.reddy7479@gmail.com>
2022-02-17 15:47:24 -0800

    updated maya's picture and changed alt text to empty string when no image was provided

--- data/impact.js
+++ data/impact.js
@@ -3,27 +3,30 @@ import introTechEthics from '../public/images/impact/events/intro-tech-ethics.pn
 import policyPitchathon21 from '../public/images/impact/events/policy-pitchathon-2021.png';
 
 export const pastEvents = [
-	{
-		title: 'Careers in Social Impact Tech',
-		quarter: 'Winter 2021',
-		img: careersSocialImpactTech,
-		alt: 'a banner titled "acm impact: careers in social impact tech, thursdays at 5pm"; features an astronaut with a graduation cap in the visor, and some planets and stars in the background',
-		description: 'We hosted a biweekly series that introduced students at UCLA to various career paths in the realm of ethical technology! Topics included open source technology, sustainable tech, and more.',
-	},
-	{
-		title: 'Intro to Tech Ethics Workshop',
-		quarter: 'Fall 2020',
-		img: introTechEthics,
-		alt: 'a banner titled "acm impact: intro to tech ethics, thursdays at 6 pm pdt; features a space shuttle and a background of planets and stars',
-		description: 'Impact’s inaugural workshop – we held open and educational discussions on topics like bias in machine learning, Big Tech regulation, and military technology.',
-	},
-	{
-		title: 'Policy Pitchathon',
-		quarter: 'Spring 2021',
-		img: policyPitchathon21,
-		alt: 'a banner titled "impact by acm at ucla: policy pitchathon, saturday april 17 10 AM - 5 PM PDT. features a judicial scale with the earth on one arm, and high-tech scroll on the other.',
-		description: 'This was a hackathon-like event in which participants joined in teams to create legislation for legal issues like data privacy and sustainability, build connections with mentors, and win prizes!',
-	},
+  {
+    title: 'Careers in Social Impact Tech',
+    quarter: 'Winter 2021',
+    img: careersSocialImpactTech,
+    alt: 'a banner titled "acm impact: careers in social impact tech, thursdays at 5pm"; features an astronaut with a graduation cap in the visor, and some planets and stars in the background',
+    description:
+      'We hosted a biweekly series that introduced students at UCLA to various career paths in the realm of ethical technology! Topics included open source technology, sustainable tech, and more.',
+  },
+  {
+    title: 'Intro to Tech Ethics Workshop',
+    quarter: 'Fall 2020',
+    img: introTechEthics,
+    alt: 'a banner titled "acm impact: intro to tech ethics, thursdays at 6 pm pdt; features a space shuttle and a background of planets and stars',
+    description:
+      'Impact’s inaugural workshop – we held open and educational discussions on topics like bias in machine learning, Big Tech regulation, and military technology.',
+  },
+  {
+    title: 'Policy Pitchathon',
+    quarter: 'Spring 2021',
+    img: policyPitchathon21,
+    alt: 'a banner titled "impact by acm at ucla: policy pitchathon, saturday april 17 10 AM - 5 PM PDT. features a judicial scale with the earth on one arm, and high-tech scroll on the other.',
+    description:
+      'This was a hackathon-like event in which participants joined in teams to create legislation for legal issues like data privacy and sustainability, build connections with mentors, and win prizes!',
+  },
 ];
 
 export const people = [
@@ -33,8 +36,8 @@ export const people = [
     position: 'Impact Director',
     major: 'Computer Science Major',
     year: 2023,
-    img: '/images/officers/MayaRaman.jpeg',
-    alt: 'Maya smiling beside royce hall',
+    img: '/images/officers/MayaRaman.jpg',
+    alt: 'Maya smiling surrounded by plants',
   },
   {
     name: 'Alyssa Wang',
@@ -106,7 +109,7 @@ export const people = [
     major: 'Computer Science and Engineering Major',
     year: 2024,
     img: '/images/officers/NoImage.jpeg',
-    alt: 'No image provided',
+    alt: '',
   },
   {
     name: 'Brendan Tremblay',
@@ -115,7 +118,7 @@ export const people = [
     major: 'Electrical Engineering Major',
     year: 2025,
     img: '/images/officers/NoImage.jpeg',
-    alt: 'No image provided',
+    alt: '',
   },
   {
     name: 'Ashley Kim',
@@ -124,42 +127,60 @@ export const people = [
     major: 'Applied Math Major',
     year: 2023,
     img: '/images/officers/NoImage.jpeg',
-    alt: 'No image provided',
+    alt: '',
   },
 ];
 
 export const blogs = [
-	{
-		title: 'Digital Tech for a Greener Future',
-		author: 'Jason Jewik',
-		date: 'Apr 3',
-		length: '11 min read',
-		img: 'https://miro.medium.com/max/1400/0*CtF08rCYE9G4Xqk7.jpg',
-		href: 'https://medium.com/acm-at-ucla/digital-tech-for-a-greener-future-7bc7f2e00bda',
-		authorImg: 'https://miro.medium.com/fit/c/96/96/1*HdS_CXUnr6GGVUfMR66_4g.jpeg',
-		authorAlt: 'a photo of Jason smiling',
-		backgroundStyle: { backgroundImage: 'url(' + 'https://miro.medium.com/max/1400/0*CtF08rCYE9G4Xqk7.jpg' + ')'},
-	},
-	{
-		title: 'Considerations for The Future of AI Governance',
-		author: 'Justin Yi',
-		date: 'Feb 10',
-		length: '6 min read',
-		img: 'https://miro.medium.com/max/1400/0*LSHF7HtyCOuJgbBP.jpg',
-		href: 'https://medium.com/impact-labs/considerations-for-the-future-of-ai-governance-46d727012c5b',
-		authorImg: 'https://www.ssu.ca/wp-content/uploads/2020/08/default-profile.png',
-		authorAlt: 'default profile pic',
-		backgroundStyle: { backgroundImage: 'url(' + 'https://miro.medium.com/max/1400/0*LSHF7HtyCOuJgbBP.jpg' + ')'},
-	},
-	{
-		title: 'What\'s up with Google\'s ethics?',
-		author: 'Maya Raman',
-		date: 'May 19',
-		length: '8 min read',
-		img: 'https://miro.medium.com/max/1400/1*uGp6b0cdPgy2fwn5dzwhdA.jpeg',
-		href: 'https://medium.com/acm-at-ucla/whats-up-with-google-s-ethics-6fd76a28e0d8',
-		authorImg: 'https://miro.medium.com/fit/c/96/96/1*vLJ5YoPEYKbDZw7OMMdXBQ.png',
-		authorAlt: 'maya in front of royce hall!',
-		backgroundStyle: { backgroundImage: 'url(' + 'https://miro.medium.com/max/1400/1*uGp6b0cdPgy2fwn5dzwhdA.jpeg' + ')'},
-	},
+  {
+    title: 'Digital Tech for a Greener Future',
+    author: 'Jason Jewik',
+    date: 'Apr 3',
+    length: '11 min read',
+    img: 'https://miro.medium.com/max/1400/0*CtF08rCYE9G4Xqk7.jpg',
+    href: 'https://medium.com/acm-at-ucla/digital-tech-for-a-greener-future-7bc7f2e00bda',
+    authorImg:
+      'https://miro.medium.com/fit/c/96/96/1*HdS_CXUnr6GGVUfMR66_4g.jpeg',
+    authorAlt: 'a photo of Jason smiling',
+    backgroundStyle: {
+      backgroundImage:
+        'url(' +
+        'https://miro.medium.com/max/1400/0*CtF08rCYE9G4Xqk7.jpg' +
+        ')',
+    },
+  },
+  {
+    title: 'Considerations for The Future of AI Governance',
+    author: 'Justin Yi',
+    date: 'Feb 10',
+    length: '6 min read',
+    img: 'https://miro.medium.com/max/1400/0*LSHF7HtyCOuJgbBP.jpg',
+    href: 'https://medium.com/impact-labs/considerations-for-the-future-of-ai-governance-46d727012c5b',
+    authorImg:
+      'https://www.ssu.ca/wp-content/uploads/2020/08/default-profile.png',
+    authorAlt: 'default profile pic',
+    backgroundStyle: {
+      backgroundImage:
+        'url(' +
+        'https://miro.medium.com/max/1400/0*LSHF7HtyCOuJgbBP.jpg' +
+        ')',
+    },
+  },
+  {
+    title: "What's up with Google's ethics?",
+    author: 'Maya Raman',
+    date: 'May 19',
+    length: '8 min read',
+    img: 'https://miro.medium.com/max/1400/1*uGp6b0cdPgy2fwn5dzwhdA.jpeg',
+    href: 'https://medium.com/acm-at-ucla/whats-up-with-google-s-ethics-6fd76a28e0d8',
+    authorImg:
+      'https://miro.medium.com/fit/c/96/96/1*vLJ5YoPEYKbDZw7OMMdXBQ.png',
+    authorAlt: 'maya in front of royce hall!',
+    backgroundStyle: {
+      backgroundImage:
+        'url(' +
+        'https://miro.medium.com/max/1400/1*uGp6b0cdPgy2fwn5dzwhdA.jpeg' +
+        ')',
+    },
+  },
 ];
