4a30f087bcb5dc1d845bdb3adbdc44f2e48efbc6
Dylon Tjanaka <dylon.tjanaka@gmail.com>
2022-03-24 20:07:21 -0700

    remove unused officers.js

--- data/index.js
+++ data/index.js
@@ -4,7 +4,6 @@ import Committees from './committees';
 import Internship from './internship';
 import Leadership from './leadership';
 import News from './news';
-import Officers from './officers';
 import Sponsors from './sponsors';
 import TGProjects from './tgprojects';
 
@@ -13,7 +12,6 @@ export default {
 	carousel: Carousel,
 	sponsors: Sponsors,
 	leadership: Leadership,
-	officers: Officers,
 	news: News,
 	tgprojects: TGProjects,
 	internship: Internship,
