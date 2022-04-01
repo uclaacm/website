fcf358d75a1c6b1463aaa457c89aa8ae01052f3f
Dylon Tjanaka <Dylon.tjanaka@gmail.com>
2022-04-01 14:37:54 -0700

    Unify officer cards. (#333)
    * Unify officer cards.
    
    * Fix lint issues.
    
    * Fix lint issues.
    
    * &nbsp;.
    
    * Remove unused css from jedi and impact modules.
    
    * lint
    
    * remove unused officers.js
    
    * add note about warning.
    
    * the fix was a missing character fml
    
    * pic --> img in jedi stuff, remove old person cardrds
    
    * square pfp
    
    * Switch back to Image because grid responsiveness was broken with img...

---
+++ components/OfficerCard.js
@@ -0,0 +1,86 @@
+import Image from 'next/image';
+import React from 'react';
+
+import styles from '../styles/components/OfficerCard.module.scss';
+
+function Officer({
+  name,
+  pronouns,
+  position,
+  /* eslint-disable-next-line no-unused-vars */
+  committee, // no officer card formats use committee yet
+  major,
+  year,
+  img,
+  alt,
+  bio,
+  size,
+  style,
+}) {
+  if (style && style.toLowerCase() === 'jedi') {
+    return (
+      <div className={styles['mb-2']}>
+        <div className={styles['jedi-profile-img']}>
+          <Image src={img} alt={alt} width={250} height={250}/>
+        </div>
+        <div className={styles['jedi-card-body']}>
+          <h2 className={styles['jedi-title']}>
+            {name} <span className={styles['pronouns-jedi']}>{pronouns}</span>
+          </h2>
+          <p className={styles['my-tight']}>{bio}</p>
+        </div>
+      </div>
+    );
+  } else if (size && size.toLowerCase() === 'compact') {
+    return (
+      <div className={styles['officer-grid-row']}>
+        <div className={styles['officer-grid-col']}>
+          <Image
+            className={styles['officer-image']}
+            src={img}
+            alt={alt}
+            width={70}
+            height={70}
+          />
+        </div>
+        <div className={`${styles['officer-grid-col']} ${styles['officer-info']}`}>
+          <h3 className={styles['officer-title']}>{name}</h3>
+          <p>{position}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`${styles['officer-card']} ${styles['grid-tablet-only-2']}`}
+      >
+        <div className={styles['officer-image-container']}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={img} alt={alt} style={{ maxWidth: '100%' }} />
+        </div>
+        <div>
+          <h3 className={styles.name}>{name}</h3>
+          <h4 className={styles.pronouns}>{pronouns}</h4>
+          <ul className="list-unstyled">
+            <li>{position}</li>
+            <li>{major}</li>
+            <li>Class of {year}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Officers(props) {
+  return (
+    // TODO: more flexible mobile views
+    <>
+      {props.officers.map((officer) => (
+        <Officer {...officer} size={props.size} style={props.style} key={props.officers.name} />
+      ))}
+    </>
+  );
+}
+
+export default Officers;
--- data/jedi.js
+++ data/jedi.js
@@ -17,19 +17,23 @@ const allyshipSpaces = [
     presenters: 'Christine Kim, Jiin Kim, Helia Woo',
     date: 'June 4, 2021',
     location: 'Online',
-    description: 'An allyship space on Western perception of kpop and other Asian media, including fetishization and otherization.',
-    slides: 'https://drive.google.com/drive/folders/16Jk212gr2jC4xJbKEkFVxwdukwF902-o',
-    pic: dontGiveThemTheAux,
-    alt: 'a slide titled "don\'t give them the aux, they\'re going to play kpop: asian representation and the otherization of kpop. features four prominent female Kpop idols.',
+    description:
+      'An allyship space on Western perception of kpop and other Asian media, including fetishization and otherization.',
+    slides:
+      'https://drive.google.com/drive/folders/16Jk212gr2jC4xJbKEkFVxwdukwF902-o',
+    img: dontGiveThemTheAux,
+    alt: "a slide titled \"don't give them the aux, they're going to play kpop: asian representation and the otherization of kpop. features four prominent female Kpop idols.",
   },
   {
     title: 'Cisgender privilege',
     presenters: 'Christine Kim',
     date: 'May 14, 2021',
     location: 'Online',
-    description: 'An allyship space on the manifestation and perpetuation of cisgender privilege.',
-    slides: 'https://drive.google.com/file/d/1m_224Uf6mj6AdYZGglnhiXgE-Mq2uJyL/view?usp=sharing',
-    pic: cisgenderPrivilege,
+    description:
+      'An allyship space on the manifestation and perpetuation of cisgender privilege.',
+    slides:
+      'https://drive.google.com/file/d/1m_224Uf6mj6AdYZGglnhiXgE-Mq2uJyL/view?usp=sharing',
+    img: cisgenderPrivilege,
     alt: 'a slide deck titled "Cisgender Privilege", with the date and time as Friday May 14th, 6:15 - 6:45 PM PDT. features an abstract sphere and ribbon.',
   },
   {
@@ -38,8 +42,9 @@ const allyshipSpaces = [
     date: 'April 30, 2021',
     location: 'Online',
     description: 'An allyship space on demonstrating responsible allyship.',
-    slides: 'https://drive.google.com/file/d/1Vs6yfcnRI-0HIuHa7VSalkHMGR_mwwld/view?usp=sharing',
-    pic: responsibleAllyship,
+    slides:
+      'https://drive.google.com/file/d/1Vs6yfcnRI-0HIuHa7VSalkHMGR_mwwld/view?usp=sharing',
+    img: responsibleAllyship,
     alt: 'a slide deck that says "a teach la allyship space: responsible allyship" with abstract shapes as the background',
   },
   {
@@ -47,9 +52,11 @@ const allyshipSpaces = [
     presenters: 'Kristie Lim, Megan Pham',
     date: 'February 26, 2021',
     location: 'Online',
-    description: 'An allyship space on housing insecurity within tech hubs, and potential solutions.',
-    slides: 'https://drive.google.com/file/d/1p6ebnqTLtXOtgJgK_Ed9emhJVeIadSye/view?usp=sharing',
-    pic: housingCrisisTechHubs,
+    description:
+      'An allyship space on housing insecurity within tech hubs, and potential solutions.',
+    slides:
+      'https://drive.google.com/file/d/1p6ebnqTLtXOtgJgK_Ed9emhJVeIadSye/view?usp=sharing',
+    img: housingCrisisTechHubs,
     alt: 'a slide titled "the housing crisis in tech hubs: kristie lim and megan pham", with a house and some trees',
   },
   {
@@ -57,9 +64,11 @@ const allyshipSpaces = [
     presenters: 'Ellie Cheng',
     date: 'February 5, 2021',
     location: 'Online',
-    description: 'An allyship space on AO3 (Archive of Our Own) and fanfiction as a safe space.',
-    slides: 'https://drive.google.com/file/d/1eV0zbnuLzySXRlfESDiH6xucTsKTHQ7W/view?usp=sharing',
-    pic: ao3,
+    description:
+      'An allyship space on AO3 (Archive of Our Own) and fanfiction as a safe space.',
+    slides:
+      'https://drive.google.com/file/d/1eV0zbnuLzySXRlfESDiH6xucTsKTHQ7W/view?usp=sharing',
+    img: ao3,
     alt: 'a simple slide with the title "AO3: a brief introduction"',
   },
   {
@@ -67,9 +76,11 @@ const allyshipSpaces = [
     presenters: 'Megha Ilango',
     date: 'January 29, 2021',
     location: 'Online',
-    description: 'An allyship space on physical forms of self-care and how to practice it.',
-    slides: 'https://drive.google.com/file/d/1eV0zbnuLzySXRlfESDiH6xucTsKTHQ7W/view?usp=sharing',
-    pic: selfCare,
+    description:
+      'An allyship space on physical forms of self-care and how to practice it.',
+    slides:
+      'https://drive.google.com/file/d/1eV0zbnuLzySXRlfESDiH6xucTsKTHQ7W/view?usp=sharing',
+    img: selfCare,
     alt: 'a slide deck that says "self-care, an allyship space by megha!" with a caption "give yourself the same care and attention that you give to others and watch yourself bloom"',
   },
   {
@@ -77,9 +88,11 @@ const allyshipSpaces = [
     presenters: 'Arjun Subramonian, Sharvani Jha',
     date: 'January 22, 2021',
     location: 'Online',
-    description: 'An allyship space on gender, intersectionality, and the importance of representation in tech.',
-    slides: 'https://drive.google.com/file/d/1Agve-hqXOZ4D0H2PnzaklDcazoND-39F/view?usp=sharing',
-    pic: qtCS,
+    description:
+      'An allyship space on gender, intersectionality, and the importance of representation in tech.',
+    slides:
+      'https://drive.google.com/file/d/1Agve-hqXOZ4D0H2PnzaklDcazoND-39F/view?usp=sharing',
+    img: qtCS,
     alt: 'a slide that says "queer + trans representation in computer science, an ACM JEDI allyship space by arjun (they/them) and sharvani (she/her). features a gradient moon.',
   },
   {
@@ -87,9 +100,11 @@ const allyshipSpaces = [
     presenters: 'Maya Raman, Vaishnavi Tipireddy, Jason Jewik',
     date: 'November 24, 2020',
     location: 'Online',
-    description: 'An allyship space on juggling academic responsibilities with personal well-being.',
-    slides: 'https://drive.google.com/file/d/10d3Q7aK4WMN4nGMvcNyvOG1ONL5RuaC2/view?usp=sharing',
-    pic: makingSchoolAChoice,
+    description:
+      'An allyship space on juggling academic responsibilities with personal well-being.',
+    slides:
+      'https://drive.google.com/file/d/10d3Q7aK4WMN4nGMvcNyvOG1ONL5RuaC2/view?usp=sharing',
+    img: makingSchoolAChoice,
     alt: 'a slide that says "Making School a Choice", with fall-themed decorations like apples and acorns',
   },
   {
@@ -97,9 +112,11 @@ const allyshipSpaces = [
     presenters: 'Arjun Subramonian, Sophie Schoenmeyer',
     date: 'November 5, 2020',
     location: 'Online',
-    description: 'An allyship space on OCD and ADHD and responding to ableism, as well as implications regarding stigma, diagnoses, and academics',
-    slides: 'https://drive.google.com/file/d/1D06JKksYWq-FsyDhAGvqjRihCTmJhFxD/view?usp=sharing',
-    pic: ocdAdhd,
+    description:
+      'An allyship space on OCD and ADHD and responding to ableism, as well as implications regarding stigma, diagnoses, and academics',
+    slides:
+      'https://drive.google.com/file/d/1D06JKksYWq-FsyDhAGvqjRihCTmJhFxD/view?usp=sharing',
+    img: ocdAdhd,
     alt: 'a simple slide that says "OCD + ADHD, Sophie Schoenmeyer and Arjun Subramonian"',
   },
   {
@@ -107,9 +124,11 @@ const allyshipSpaces = [
     presenters: 'Eugene Lo, Matt Wang',
     date: 'November 17, 2020',
     location: 'Online',
-    description: 'An allyship space on improving mental and physical health and seeking support systems.',
-    slides: 'https://drive.google.com/file/d/1r9zEz5O8hLFaXtj_pOdMhjd9pgSzGkof/view?usp=sharing',
-    pic: healthyLiving,
+    description:
+      'An allyship space on improving mental and physical health and seeking support systems.',
+    slides:
+      'https://drive.google.com/file/d/1r9zEz5O8hLFaXtj_pOdMhjd9pgSzGkof/view?usp=sharing',
+    img: healthyLiving,
     alt: 'a simple cover slide with the title "healthy living: eugene and matt\'s jedi allyship presentation", and some ferns',
   },
   {
@@ -117,9 +136,11 @@ const allyshipSpaces = [
     presenters: 'Ashley Ghodsian',
     date: 'October 27, 2020',
     location: 'Online',
-    description: 'An allyship space on disability, ableist culture, and what can tech do to improve accessibility.',
-    slides: 'https://drive.google.com/file/d/1baLskai0nIdJJESyieke3z4WkzNu3OTK/view?usp=sharing',
-    pic: disabilityAccessibility,
+    description:
+      'An allyship space on disability, ableist culture, and what can tech do to improve accessibility.',
+    slides:
+      'https://drive.google.com/file/d/1baLskai0nIdJJESyieke3z4WkzNu3OTK/view?usp=sharing',
+    img: disabilityAccessibility,
     alt: 'the cover slide for the talk, with a silhouette of a person sitting with a laptop on their lap and a chat bubble next to their head.',
   },
 ];
@@ -128,83 +149,137 @@ const jedis = [
   {
     name: 'helia',
     pronouns: 'she/her',
-    bio: <>Helix but with an &quot;a&quot; except for when I order food then I am Bob the Builder
-    <br/><b>red flag:</b> likes using 1 ply toilet paper</>,
-    pic: '/images/jedi/jedi-officers/helia.jpg',
+    bio: (
+      <>
+        Helix but with an &quot;a&quot; except for when I order food then I am
+        Bob the Builder
+        <br />
+        <b>red flag:</b> likes using 1 ply toilet paper
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/helia.jpg',
     alt: 'helia pointing at a sign saying "Women"',
   },
   {
     name: 'archie',
     pronouns: 'she/they',
-    bio: <>I cried over finding out my friend has a twin sister
-    <br/><b>red flag:</b> will kick you out of the Sproul Cove 9th floor lounge</>,
-    pic: '/images/jedi/jedi-officers/archie.jpeg',
+    bio: (
+      <>
+        I cried over finding out my friend has a twin sister
+        <br />
+        <b>red flag:</b> will kick you out of the Sproul Cove 9th floor lounge
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/archie.jpeg',
     alt: 'archie in a dress in a backyard',
   },
   {
     name: 'christine',
     pronouns: 'they/them',
-    bio: <>has based taste in everything<br/>
-    <b>red flag:</b> cuwustine!!!! 🥺🥺 🚩!</>,
-    pic: '/images/jedi/jedi-officers/christine.jpg',
+    bio: (
+      <>
+        has based taste in everything
+        <br />
+        <b>red flag:</b> cuwustine!!!! 🥺🥺 🚩!
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/christine.jpg',
     alt: 'christine wearing a scarf and smiling at the camera',
   },
   {
     name: 'maya',
     pronouns: 'she/???',
-    bio: <>ask me about my egg flipping videos
-    <br/><b>red flag:</b> reads AOT fanfiction</>,
-    pic: '/images/jedi/jedi-officers/maya.jpg',
+    bio: (
+      <>
+        ask me about my egg flipping videos
+        <br />
+        <b>red flag:</b> reads AOT fanfiction
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/maya.jpg',
     alt: 'maya in overalls standing in front of an art piece with a threatening expression',
   },
   {
     name: 'jiin',
     pronouns: 'she/her',
-    bio: <>my labels are INTP 5w4 sp/sx Ravenclaw airbender
-    <br/><b>red flag:</b> is sus</>,
-    pic: '/images/jedi/jedi-officers/jiin.jpg',
+    bio: (
+      <>
+        my labels are INTP 5w4 sp/sx Ravenclaw airbender
+        <br />
+        <b>red flag:</b> is sus
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/jiin.jpg',
     alt: 'jiin wearing an animal hat at Joshua Tree',
   },
   {
     name: 'paymon',
     pronouns: 'he/him',
-    bio: <>the only time I’ve gotten into a car accident is at a McDonalds drive thru at 1 AM<br/>
-    <b>red flag:</b> will hit your car at the McDonalds drive thru at 1 AM</>,
-    pic: '/images/jedi/jedi-officers/paymon.png',
+    bio: (
+      <>
+        the only time I’ve gotten into a car accident is at a McDonalds drive
+        thru at 1 AM
+        <br />
+        <b>red flag:</b> will hit your car at the McDonalds drive thru at 1 AM
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/paymon.png',
     alt: 'paymon smiling in a park!',
   },
   {
     name: 'stephen',
     pronouns: 'he/him',
-    bio: <>I have never bought an eraser in my life, I just keep picking them up off the floor<br/>
-    <b>red flag:</b> hates Rodrick Heffley</>,
-    pic: '/images/jedi/jedi-officers/stephen.jpg',
+    bio: (
+      <>
+        I have never bought an eraser in my life, I just keep imgking them up
+        off the floor
+        <br />
+        <b>red flag:</b> hates Rodrick Heffley
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/stephen.jpg',
     alt: 'bald stephen staring scarily while a flashlight is pointed at his face from underneath',
   },
   {
     name: 'edmond',
     pronouns: 'he/they',
-    bio: <>from: temple city, ca (near pasadena) RED FLAG…<br/>
-        likes: cats, cooking, Speedy <br/>
-        likes: cooking cats speedily<br/>
-        likes: cooking speedy cats<br/>
-        likes: cats speedily cooking<br/>
-        red flag: did high school debate<br/>
-        dislikes: dave matthews band<br/>
-        likes: matthew<br/>
-        dislikes: dave<br/>
-        neutral: band<br/>
-        fun fact: shares birthday with mom<br/>
-        unwilling to accept the existence of dave matthews (i play jazz steel guitar apparently)<br/>
-        red flag: plays league :barf_emoji: RED FALGSDFSDFSDKF :barf: :barf:
-y</>,
-    pic: '/images/jedi/jedi-officers/edmond.jpg',
+    bio: (
+      <>
+        from: temple city, ca (near pasadena) RED FLAG…
+        <br />
+        likes: cats, cooking, Speedy <br />
+        likes: cooking cats speedily
+        <br />
+        likes: cooking speedy cats
+        <br />
+        likes: cats speedily cooking
+        <br />
+        red flag: did high school debate
+        <br />
+        dislikes: dave matthews band
+        <br />
+        likes: matthew
+        <br />
+        dislikes: dave
+        <br />
+        neutral: band
+        <br />
+        fun fact: shares birthday with mom
+        <br />
+        unwilling to accept the existence of dave matthews (i play jazz steel
+        guitar apparently)
+        <br />
+        red flag: plays league :barf_emoji: RED FALGSDFSDFSDKF :barf: :barf: y
+      </>
+    ),
+    img: '/images/jedi/jedi-officers/edmond.jpg',
     alt: 'edmond in front of some flowers',
   },
 ];
 
-const blogLink = 'https://medium.com/acm-at-ucla/may-the-force-be-with-acm-at-ucla-49e9258d71e1';
+const blogLink =
+  'https://medium.com/acm-at-ucla/may-the-force-be-with-acm-at-ucla-49e9258d71e1';
 
 const contactEmail = 'helia.woo@ucla.edu';
 
-export { allyshipSpaces, blogLink, contactEmail, jedis};
+export { allyshipSpaces, blogLink, contactEmail, jedis };
