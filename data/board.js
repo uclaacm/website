ed7ea59c88e1d933308032e3352cc46899350aad
jainsujay02 <sujayjain2121@gmail.com>
2022-10-09 23:38:41 -0700

    ACM Fall 2022 Internship Application Page Added
    Committee and board descriptions updated.
    New Application links added.

--- data/board.js
+++ data/board.js
@@ -36,18 +36,19 @@ export default [
                 <li> The Dev team interns will train and maintain existing ACM projects
                     (website, discord bot, membership portal), adding features and dealing with issues</li>
                 <li> The Impact interns will plan Impact events and help out with the blog</li>
+                <li> JEDI interns will work with the JEDI director to organize events, collaborate with the dev team to work on projects like the Anti-Resume, and conduct the ACM culture survey.</li>
                 <li> The External Affairs interns will work with companies and plan out events
                     such as panels, company tours, and info-sessions</li>
                 <li> The Finance interns will help draft various finance requests to send to the CS department
                     and work with committees to secure funding for events</li>
                 <li> The Alumni intern will work with alumni to organize panels and other events. Assist with a
                     quarterly alumni newsletter and alumni social media groups.</li>
-                <li> The Marketing intern will Work with the marketing team to advertise general ACM events, maintain
+                <li> The Marketing interns will work with the marketing team to advertise general ACM events, maintain
                     ACM’s online presence, and develop strategies for marketing and publicity. Assist with ACM’s social
                     media and weekly newsletter.</li>
-                <li> Moonshots Interns will work with their advisor and ACM leadership on a self-driven project of their
-                    choice; examples include transfer advocacy and support, cross-organization collaboration, and new
-                    committee or event ideas. See <a href="https://tinyurl.com/ACMInternship2021" target="_blank" rel="noopener noreferrer">the committee descriptions</a> for an expanded blurb!</li>
+                <li> Photography interns will work with the marketing team to take pictures, record memories of ACM’s events, and take headshots for ACM officers and interns.</li>
+                <li> Transfer Affairs Intern Coordinates events focused on engaging the transfer community and help connect transfer students to resources within the CS community at UCLA.</li>
+
             </ul> ),
             card: '/images/cards/board_card.svg',
             nextSteps: 'Dev Team interns have the opportunity to become Dev Team officers in the spring. All other Interns will run for a director position in the Spring or will no longer officially be a member of the board.',
