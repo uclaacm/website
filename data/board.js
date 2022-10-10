3d819ce1ae99872bbafca363b8a10a4eb1b83ae6
jainsujay02 <sujayjain2121@gmail.com>
2022-10-10 00:12:03 -0700

    Correction to Error

--- data/board.js
+++ data/board.js
@@ -29,17 +29,17 @@ export default [
             </> ),
             whatInternsWillDo: (
             <ul>
-                <li> Operations Interns will work on and lead important all-ACM events such as Winter GM and Faculty Fireside. </li>
-                <li> Internal Affairs interns will organize fun socials to bring officers across different committees together.</li>
-                <li> The Dev team interns will train and maintain existing ACM projects, adding features and dealing with issues.</li>
+                <li> Operations Interns will lead important all-ACM events such as Winter GM. </li>
+                <li> Internal Affairs interns will organize fun socials across committees.</li>
+                <li> The Dev team interns will maintain existing ACM projects and add features. </li>
                 <li> The Impact interns will plan Impact events and help out with the blog</li>
-                <li> JEDI interns will organize events, work on projects like the Anti-Resume, and conduct the ACM culture survey.</li>
-                <li> External Affairs interns will work with companies and plan out events such as panels, company tours, and info-sessions</li>
-                <li> Finance interns will draft finance requests to send to the CS department and secure funding for events</li>
-                <li> Alumni intern will organize panels and other events. Assist with a quarterly alumni newsletter and alumni social media groups.</li>
-                <li>  Marketing interns will advertise general ACM events, maintain ACM’s online presence, and develop marketing strategies.</li>
-                <li> Photography interns will  take pictures, record memories of ACM’s events, and take headshots for ACM officers and interns.</li>
-                <li> Transfer Affairs Intern will coordinate events focused on engaging the transfer community at UCLA.</li>
+                <li> JEDI interns will organize events, work on projects and conduct culture survey.</li>
+                <li> External Affairs interns will plan out events like company tours and info-sessions</li>
+                <li> Finance interns will draft finance requests and secure funding for events</li>
+                <li> Alumni intern will organize panels and alumni social media groups.</li>
+                <li> Marketing interns will advertise general ACM events and maintain ACM's online presence.</li>
+                <li> Photography interns will record memories of ACM’s events, and take headshots.</li>
+                <li> Transfer Affairs Intern will coordinate events to engage the transfer community at UCLA.</li>
 
             </ul> ),
             card: '/images/cards/board_card.svg',
