2136b411fc720ec0e7f810ef287a09fde0d8aca1
Amy He <amyh11325@gmail.com>
2023-01-15 15:02:47 -0800

    Add recurring event parser

--- scripts/event-generator-sheets.mjs
+++ scripts/event-generator-sheets.mjs
@@ -8,61 +8,23 @@ dotenv.config();
 const SPREADSHEET_ID = process.env.EVENTS_SPREADSHEET_ID;
 const SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT ?? '';
 
-// Grab all single events from spreadsheet
-// and write to output.json
-async function writeAllSingleEvents() {
-  // Get all events
-  let events = [];
-  for (let i = 1; i <= 10; i++) {
-    let temp = await getEventsOfWeek(i);
-    events = events.concat(temp);
-  }
+// Week one MONDAY of the quarter (y, m (base 0), d)
+const FIRST_DAY_OF_QUARTER = new Date(2023, 0, 9);
+const DAYS_OF_WEEK = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
+// Grab all single and recurring events of Week n
+// and write to output.json
+async function writeAllEventsOfWeek(n) {
+  // Get events
+  let events = (await getSingleEventsOfWeek(n)).concat(await getRecurringEventsOfWeek(n));
   const cleaned = events.filter((item) => item);
   writeToOutput(cleaned);
 }
 
-// write events (list of event jsons) to output.json
-function writeToOutput(events) {
-  // Write to output.json
-  const out = JSON.stringify(events);
-  fs.writeFile('output.json', out, (err) => {
-    if (err) throw err;
-    // eslint-disable-next-line no-console
-    console.log('Output successfully saved to output.json');
-  });
-}
-
-// Read events of Week n
+// Read single events of Week n
 // Return as array of JSON objects
-async function getEventsOfWeek(n) {
-  const sheets = google.sheets({version: 'v4'});
-
-  // Get JWT Token to access sheet
-  const service_account = JSON.parse(SERVICE_ACCOUNT);
-  const jwtClient = new google.auth.JWT(
-    service_account.client_email,
-    '',
-    service_account.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets'],
-  );
-  jwtClient.authorize(function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-
-  // Get data from Google spreadsheets
-  const res = await sheets.spreadsheets.values.get({
-    auth: jwtClient,
-    spreadsheetId: SPREADSHEET_ID,
-    range: 'Week ' + n + '!A:H',
-  });
-  const rows = res?.data.values;
-  if(!rows || rows.length == 0) {
-    console.log('Error: no data found');
-    return [];
-  }
+async function getSingleEventsOfWeek(n) {
+  const rows = await getGoogleSheetData('Week ' + n + '!A:H');
 
   const events = [];
   for (const row of rows) {
@@ -92,4 +54,94 @@ async function getEventsOfWeek(n) {
   return events;
 }
 
-writeAllSingleEvents();
+// Read recurring events of Week n
+// Return as array of JSON objects
+async function getRecurringEventsOfWeek(n) {
+  const rows = await getGoogleSheetData('RECURRING EVENTS!A:J');
+
+  const events = [];
+  for (const row of rows) {
+    // Skip header rows and example event
+    if (row.length < 5 || row[0] === 'Committee' || row[0].includes('Example:')) {
+      continue;
+    }
+
+    // Check the current week is within the event's range
+    if (parseInt(row[2]) <= n && parseInt(row[3]) >= n) {
+      try {
+        // Calculate date of event for this week
+        const d = (n - 1) * 7 + DAYS_OF_WEEK.indexOf(row[4].toLowerCase());
+        const date = FIRST_DAY_OF_QUARTER;
+        date.setDate(date.getDate() + d);
+
+        events.push(generateSingleEvent({
+          title: row[1],
+          start: null,
+          end: null,
+          committee: getCssStringFromCommittee(row[0]),
+          location: row[6],
+          description: row[7],
+          links: null,
+          time: row[5],
+          date: date.toISOString().split('T')[0],
+          fblink: row[8]}));
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(`Error ${err} on event ${row}`);
+      }
+    }
+  }
+
+  return events;
+}
+
+////////////////////////////////////////////////////////
+// Helper Functions
+////////////////////////////////////////////////////////
+
+// Read data from Google sheets
+// using sheet range (eg: 'Week 1!A:H)
+async function getGoogleSheetData(range) {
+  const sheets = google.sheets({version: 'v4'});
+
+  // Get JWT Token to access sheet
+  const service_account = JSON.parse(SERVICE_ACCOUNT);
+  const jwtClient = new google.auth.JWT(
+    service_account.client_email,
+    '',
+    service_account.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets'],
+  );
+  jwtClient.authorize(function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+
+  // Get data from Google spreadsheets
+  const res = await sheets.spreadsheets.values.get({
+    auth: jwtClient,
+    spreadsheetId: SPREADSHEET_ID,
+    range: range,
+  });
+  const rows = res?.data.values;
+  if(!rows || rows.length == 0) {
+    console.log('Error: no data found');
+    return [];
+  }
+
+  return rows;
+}
+
+// write events (list of event jsons) to output.json
+function writeToOutput(events) {
+  // Write to output.json
+  const out = JSON.stringify(events);
+  fs.writeFile('output.json', out, (err) => {
+    if (err) throw err;
+    // eslint-disable-next-line no-console
+    console.log('Output successfully saved to output.json');
+  });
+}
+
+writeAllEventsOfWeek(1);
