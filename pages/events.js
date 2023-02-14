4815ea431b298f5b0f8b704de20fbf0e3831ce28
Amy He <amyh11325@gmail.com>
2023-02-13 20:32:56 -0800

    Add new lines and links to events calendar

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,4 @@
+import DOMPurify from 'isomorphic-dompurify';
 import moment from 'moment';
 import { NextSeo } from 'next-seo';
 import React, { useState } from 'react';
@@ -93,16 +94,20 @@ function Events({ events }) {
 	);
 }
 
+
 export const getStaticProps = async () => {
 	const events = await getAllEvents();
-	// Attempt to replace new lines with <br/>, doesnt work
-	// const processedEvents = events.map((event) => (
-	// 	{...event, description: <>{event.description.replace(/\n/g, '<br/>')}</>}));
-	// console.log(processedEvents);
+	// I copied this from geeksforgeeks https://www.geeksforgeeks.org/how-to-replace-plain-url-with-link-using-javascript/
+	const linkRegex = /(\b(https?|ftp|file):\/\/([-A-Z0-9+&@#%?=~_|!:,.;]*)([-A-Z0-9+&@#%?\/=~_|!:,.;]*)[-A-Z0-9+&@#\/%=~_|])/ig;
+
+	const lineBreakEvents = events.map((event) => ({...event, description: event.description.replace(/(?:\r\n|\r|\n)/g, '<br/>')}));
+	const linkedEvents = lineBreakEvents.map((event) => ({...event, description: event.description.replace(linkRegex, "<a href='$1' target='_blank'>$3</a>")}));
+	const processedEvents = linkedEvents.map((event) => (
+		{...event, description: (DOMPurify.sanitize(event.description))}));
 
 	return {
 		props: {
-			events: events,
+			events: processedEvents,
 		},
 
 		revalidate: 3600,
