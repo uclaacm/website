d195fff5833bbd9547c64104f12a125fafaa3dcf
Arsh <arshmalik02@gmail.com>
2023-04-16 15:54:24 -0700

    Update banner on landing page
    ContentBanner.js: Edit component to display only text
    index.js: add banner text
    package.json: add postcss dep

--- components/ContentBanner.js
+++ components/ContentBanner.js
@@ -1,21 +1,11 @@
-import Link from 'next/link';
 import React from 'react';
 
-function ContentBanner({ title, ctaText, ctaLink }) {
+function ContentBanner({ title }) {
     return (
         <div className="full-width blue">
-            <div className="sign-up-section banner-section">
-                <div id="banner-left" className="half-width">
-                    <h2 style={{fontSize: '1.6em'}}>{title}</h2>
-                </div>
-                <div id="banner-right" className="half-width">
-                    <Link href={ctaLink}>
-                        <a className="button tight dark">
-                            {ctaText}
-                        </a>
-                    </Link>
-                </div>
-			</div>
+            <div id="banner-right" className="full-width">
+                <h2 style={{fontSize: '1.6em'}}>{title}</h2>
+             </div>
 		</div>
     );
 }
