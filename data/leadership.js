ba3e2978736e8821eb2b93abca4b341a86e23918
Anmol Gupta <anmolgupta23@ucla.edu>
2023-04-27 09:35:57 -0700

    Update PVP/CPrez/Dev Team Profiles

--- data/leadership.js
+++ data/leadership.js
@@ -1,87 +1,97 @@
 export default [
-	{
-		name: 'Ava Asmani',
-		position: 'President',
-		img: '/images/officers/AvaAsmani.jpg',
-	},
-	{
-		name: 'Zack Pakin',
-		position: 'External Vice President',
-		img: '/images/officers/ZackPakin.jpg',
-	},
-	{
-		name: 'Sujay Jain',
-		position: 'Internal Vice President',
-		img: '/images/officers/SujayJain.jpg',
-	},
-	{
-		name: 'Christina Tong',
-		position: 'Co-President, Hack',
-		img: '/images/officers/ChristinaTong.jpg',
-	},
-	{
-		name: 'Nareh Agazaryan',
-		position: 'Co-President, Hack',
-		img: '/images/officers/NarehAgazaryan.jpg',
-	},
-	{
-		name: 'Aman Oberoi',
-		position: 'President, AI',
-		img: '/images/officers/AmanOberoi.jpg',
-	},
-	{
-		name: 'Paige Brown',
-		position: 'President, ACM-W',
-		img: '/images/officers/PaigeBrown.jpg',
-	},
-	{
-		name: 'Robert Lee',
-		position: 'President, ICPC',
-		img: '/images/officers/RobertLee.jpg',
-	},
-	{
-		name: 'Ray Hsiao',
-		position: 'Co-President, Game Studio',
-		img: '/images/officers/RayHsiao.jpg',
-	},
-	{
-		name: 'Caroline Wang',
-		position: 'Co-President, Game Studio',
-		img: '/images/officers/CarolineWang.jpg',
-	},
-	{
-		name: 'Jerry Xu',
-		position: 'Co-President, Cyber',
-		img: '/images/officers/JerryXu.jpg',
-	},
-	{
-		name: 'Stephen Kelman',
-		position: 'Co-President, Cyber',
-		img: '/images/officers/StephenKelman.jpg',
-	},
-	{
-		name: 'Milo Kearney',
-		position: 'General President, Teach LA',
-		img: '/images/officers/MiloKearney.jpg',
-	},
-	{
-		name: 'Eden Yu',
-		position: 'Vice President, Teach LA',
-		img: '/images/officers/EdenYu.jpg',
-	},
-	{
-		name: 'Jiin Kim',
-		position: 'Dev President, Teach LA',
-		img: '/images/officers/JiinKim.jpg',
-	},
-	{
-		name: 'Muthu Palaniappan',
-		position: 'Co-President, Design',
-		img: '/images/officers/MuthuPalaniappan.jpg',
-	},
-	{
-		name: 'Ivy Kang',
-		position: 'Co-President, Design',
-		img: '/images/officers/IvyKang.JPG',
-	},
+  {
+    name: 'Sujay Jain',
+    position: 'Internal Vice President',
+    img: '/images/officers/SujayJain.jpg',
+  },
+  {
+    name: 'Eavan Jael Soriano',
+    position: 'External Vice President',
+    img: '/images/officers/EavanJaelSoriano.jpg',
+  },
+  {
+    name: 'Advit Deepak',
+    position: 'Internal Vice President',
+    img: '/images/officers/AdvitDeepak.jpg',
+  },
+  {
+    name: 'James Wu',
+    position: 'Co-President, Hack',
+    img: '/images/officers/JamesWu.jpg',
+  },
+  {
+    name: 'Nathan Zhang',
+    position: 'Co-President, Hack',
+    img: '/images/officers/NathanZhang.jpg',
+  },
+  {
+    name: 'Jordan Lin',
+    position: 'Co-President, AI',
+    img: '/images/officers/JordanLin.jpg',
+  },
+  {
+    name: 'Angeline Xu',
+    position: 'Co-President, AI',
+    img: '/images/officers/acmlogocopy.jpg',
+  },
+  {
+    name: 'Evy Nguyen',
+    position: 'Co-President, ACM-W',
+    img: '/images/officers/EvyNguyen.jpg',
+  },
+  {
+    name: 'Veronica Salas',
+    position: 'Co-President, ACM-W',
+    img: '/images/officers/VeronicaSalas.jpg',
+  },
+  {
+    name: 'Suhas Nagar',
+    position: 'President, ICPC',
+    img: '/images/officers/SuhasNagar.jpg',
+  },
+  {
+    name: 'Henry Wang',
+    position: 'Co-President, Game Studio',
+    img: '/images/officers/HenryWang.jpg',
+  },
+  {
+    name: 'Ming Zhu',
+    position: 'Co-President, Game Studio',
+    img: '/images/officers/MingZhu.jpg',
+  },
+  {
+    name: 'Benson Liu',
+    position: 'Co-President, Cyber',
+    img: '/images/officers/BensonLiu.jpg',
+  },
+  {
+    name: 'Salma Alandary',
+    position: 'Co-President, Cyber',
+    img: '/images/officers/SalmaAlandary.jpg',
+  },
+  {
+    name: 'Sruthi Rangarajan',
+    position: 'Co-President, Teach LA',
+    img: '/images/officers/SruthiRangarajan.jpg',
+  },
+  {
+    name: 'Joseph Ramirez',
+    position: 'Co-President, Teach LA',
+    img: '/images/officers/JosephRamirez.jpg',
+  },
+  {
+    name: 'Alyssa Tadeo',
+    position: 'Dev President, Teach LA',
+    img: '/images/officers/AlyssaTadeo.jpg',
+  },
+  {
+    name: 'William Hsieh',
+    position: 'Co-President, Design',
+    img: '/images/officers/WilliamHsieh.jpg',
+  },
+  {
+    name: 'Michael Shi',
+    position: 'Co-President, Design',
+    img: '/images/officers/acmlogocopy.jpg',
+  },
 ];
