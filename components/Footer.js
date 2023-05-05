bfa47fe5649079ef3e72374ecebdccddb12fb155
Arsh <arshmalik02@gmail.com>
2023-05-05 12:52:01 -0700

    Add S23 Town Hall Page Footer.js: update town hall page link s23.js: create a new page for Spring 23 town hall

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ const footerACMLinks = [
 	{ title: 'About', path: '/about' },
 	{ title: 'Events', path: '/events' },
 	{ title: 'General Meeting', path: '/gm/w23' },
-	{ title: 'CS Town Hall', path: '/town-hall/f22' },
+	{ title: 'CS Town Hall', path: '/town-hall/s23' },
 	{ title: 'Internship Program', path: '/internship' },
 	{ title: 'Dev Team', path: '/dev'},
 	{ title: 'Sponsors', path: '/sponsors' },
