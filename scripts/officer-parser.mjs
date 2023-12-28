24e02417e3cfb1ef3c7f3ddc409b50d672b56cec
larryz1230 <larryzhi1230@gmail.com>
2023-12-27 16:05:34 -0800

    officer script works

--- scripts/officer-parser.mjs
+++ scripts/officer-parser.mjs
@@ -9,6 +9,8 @@ const SERVICE_ACCOUNT = process.env["SERVICE_ACCOUNT"];
 const DIRECTORY_SPREADSHEET_ID = process.env["DIRECTORY_SPREADSHEET_ID"];
 
 
+// await getGoogleSheetData('Officers!A2:K');
+writeToOutput(await getGoogleSheetData('Officers!A2:K'));
 
 async function getAllOfficers() {       //tofix
     // Get all single events
@@ -39,47 +41,80 @@ async function getAllOfficers() {       //tofix
 // Read data from Google sheets
 // using sheet range (eg: 'Week 1!A:H)
 async function getGoogleSheetData(range) {
-    const sheets = google.sheets({version: 'v4'});
-  
-    // Get JWT Token to access sheet
-    const service_account = JSON.parse(SERVICE_ACCOUNT);
-    const jwtClient = new google.auth.JWT(
-      service_account.client_email,
-      '',
-      service_account.private_key,
-      ['https://www.googleapis.com/auth/spreadsheets'],
-    );
-    jwtClient.authorize(function (err) {
-      if (err) {
-        throw err;
-      }
-    });
-  
-    // Get data from Google spreadsheets
+  const sheets = google.sheets({ version: 'v4' });
+
+  // Get JWT Token to access sheet
+  const service_account = JSON.parse(SERVICE_ACCOUNT);
+  const jwtClient = new google.auth.JWT(
+    service_account.client_email,
+    null, // or undefined, or an empty string (depends on your use case)
+    service_account.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets'],
+  );
+
+  // Authorize the client
+  await jwtClient.authorize();
+
+  const committees = ["Board, Internal", "Board, External", "AI", "Cyber", "Design", "Studio", "Hack", "ICPC", "Teach LA", "W"];
+
+  // Get data from Google spreadsheets
+  try {
     const res = await sheets.spreadsheets.values.get({
       auth: jwtClient,
       spreadsheetId: DIRECTORY_SPREADSHEET_ID,
       range: range,
     });
+
+    
+    let currCommittee = "President";
+    let offs = [];
     const rows = res?.data.values;
-    if(!rows || rows.length == 0) {
-      console.log('Error: no data found');
-      return [];
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
+      if (committees.includes(row[0])){
+        currCommittee = row[0];
+      } else {
+        row[11] = currCommittee;
+        offs.push(row);
+      }
+      
+      // console.log(`Row ${i + 1}: ${row[10]}`);
     }
-  
-    // // Replacing the new lines with <br/> (doesnt work tho)
-    // const formatRows = rows.map((row) => row.map( (r) => r.replace(/\n/g, '<br/>')));
-    // return formatRows;
-  
-    return rows;
+
+    // if (!rows || rows.length === 0) {
+    //   console.log('Error: no data found');
+    //   return [];
+    // }
+
+    // Format the rows into an array of objects
+    const formattedData = offs.map((row) => ({
+      role: row[0],
+      name: row[1],
+      pronouns: row[2],
+      email: row[3],
+      phone: row[4],
+      year: row[5],
+      major: row[6],
+      birthday: row[7],
+      discord: row[8],
+      github: row[9],
+      photo: row[10],
+      committee: row[11],
+    }));
+    console.log(formattedData);
+    return formattedData;
+  } catch (error) {
+    console.error('Error retrieving data from Google Sheets:', error.message);
+    return [];
   }
+}
 
   function writeToOutput(officers) {
-    // Write to output.json
+    // Write to offoutput.json
     const out = JSON.stringify(officers);
-    fs.writeFile('output.json', out, (err) => {
+    fs.writeFile('offoutput.json', out, (err) => {
       if (err) throw err;
       // eslint-disable-next-line no-console
-      console.log('Output successfully saved to output.json');
+      console.log('Output successfully saved to offoutput.json');
     });
   }
