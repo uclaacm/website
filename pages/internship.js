0fb20860137a4ed41a9001e74427ef9314fa10b2
sgabrani <sahitigabrani@gmail.com>
2024-01-25 17:58:01 -0800

    update internship - apps closed

--- pages/internship.js
+++ pages/internship.js
@@ -47,11 +47,9 @@ function InternshipPage(){
             <div className={'content-section text-center'}>
                 <h1>ACM Internship Program</h1>
                 {/* eslint-disable-next-line max-len */}
-                <h2>Applications for the ACM Internship Program are now live. <br></br><a href="http://tinyurl.com/ACM-W24-apps" target="_blank" rel="noopener noreferrer">Apply Now </a></h2>
+                <h2>Applications for the ACM Internship Program are now closed. <br></br>Check back later for updates!</h2>
                 <br></br>
                 <p className={styles['internship-intro']}>The ACM Internship program is a program that facilitates the transition of UCLA students to ACM officers. Interns learn about the ACM committee they are interning for and ACM in general before becoming an official ACM officer.</p>
-                <p className={styles['internship-intro-highlight']}>The following committees are recruiting in the upcoming cycle:</p>
-                <p className={styles['internship-intro-highlight']}>ACM Cloud and ACM Board (JEDI, Alumni, and Transfer Affairs)</p>
                 <p className={styles['internship-intro']}>Click on the different ACM subcommittee icons in order to learn more about what interns in each committee do and scroll down to find more information on how you can apply to the Internship Program.</p>
             </div>
             <CommitteeInternshipInfo
