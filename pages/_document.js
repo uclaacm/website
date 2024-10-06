09d2433c10b8cb27487ea4f9254b6a430745c8cf
Snigdha Kansal <snigdha0206@g.ucla.edu>
2024-10-06 15:53:23 -0700

    add mailchimp popup script

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,11 @@ class AppDocument extends Document {
           <link href={'/images/logo.png'} rel="icon" type="image/png" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
           <meta name="theme-color" content="#1E6CFF" />
+          <script
+            id="mcjs"
+            src="https://chimpstatic.com/mcjs-connected/js/users/5666284d62e955e1bc84f7ba0/4a4c455ee0b05fb0493dc86b2.js"
+            async
+          />
         </Head>
         <body>
           <Main />
