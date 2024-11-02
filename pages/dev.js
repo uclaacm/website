70dfb79bb5a0fbb9c2593c8f549457f8ab0f14bc
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2024-11-01 20:07:27 -0700

    Acm dev cards (#717)
    * projects changed to cards, styling needed though
    
    * Description updated, need to get color span working
    
    * Added color dots to language, as well as images
    and some formatting changes
    
    * Style Updates
    
    * scss updates
    
    * Update Projects database + Advisor profile picture, as well as minor CSS changes
    
    * general formatting, scss and import fixes
    
    * lint formatting fixes
    
    * disable style linter for dynamically created content
    
    * nomenclature update
    
    * image updates & link updates
    
    * CSS & Image updates to clean up PR.
    
    * removed a project + no proj/repo link for ??? proj
    
    * misspellings & removing extra spaces
    
    * fixed var-name

--- pages/dev.js
+++ pages/dev.js
@@ -3,24 +3,25 @@ import Link from 'next/link';
 import React from 'react';
 
 import Banner from '../components/Banner';
+import Projects from '../components/DevProjectCards';
 import Layout from '../components/Layout';
 import Officers from '../components/OfficerCard';
 
+import {projects} from '../data/dev';
 import data from '../offoutput.json';
-
 import styles from '../styles/pages/Dev.module.scss';
 
-const devTeamDirector = {
+/*const devTeamDirector = {
 	name: 'Snigdha Kansal',
 	email: 'snigdha0206@g.ucla.edu',
-};
+};*/
 
 const devTeamAdvisor = {
 	role: 'Dev Team Advisor',
 	name: 'Arsh Malik',
 	pronouns: 'he/him/his',
 	year: '2025', major: 'Computer Science',
-	photo: 'https://drive.google.com/thumbnail?id=1LWWf0NdOuVjWsxPkXG7e7ki135KfCAq7',
+	photo: 'https://drive.google.com/thumbnail?id=1d1wYxOHHS5hGzC4xOU9_39sUFb1xxSv7',
 };
 
 function DevTeam() {
@@ -29,12 +30,12 @@ function DevTeam() {
 	return (
 		<Layout>
 			<NextSeo
-				title="ACM Dev Team | ACM at UCLA"
-				description="The ACM Dev Team handles general internal development needs for ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and link shortener."
+				title='ACM Dev Team | ACM at UCLA'
+				description='The ACM Dev Team handles general internal development needs for ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and link shortener.'
 			/>
 			<Banner decorative />
 			<div className={'content-section text-center'}>
-				<h1 className="text-center">Dev Team</h1>
+				<h1 className='text-center'>Dev Team</h1>
 				<p className={styles['dev-team-info']}>
 					{/* eslint-disable-next-line max-len */}
 					The ACM Dev Team handles general internal development needs for all of ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and link shortener.
@@ -45,47 +46,11 @@ function DevTeam() {
 						<a>apply to intern</a>
 					</Link>&nbsp;with the Dev Team!
 				</p>
-				<h2 className="text-center">Our Projects</h2>
-				<div className='text-left'>
-					<h3 id={styles['first-project']}>ACM at UCLA Website</h3>
-						<p className={styles['project-info']}>The&nbsp;
-							<Link href='https://www.uclaacm.com'>
-								<a target='_blank' rel='noopener noreferrer'>ACM at UCLA Website</a>
-							</Link>
-							{/* eslint-disable-next-line max-len */}
-							&nbsp;is the one-stop shop for all things ACM! We develop and maintain this website, keeping it up-to-date with all the latest news and events. In fact, we made the page you&apos;re looking at right now! This website is built with React.js and Next.js and deployed with Netlify - if you&apos;re curious, feel free to check out the&nbsp;
-							<Link href='https://www.github.com/uclaacm/website'>
-								<a target='_blank' rel='noopener noreferrer'>source code</a>
-							</Link>
-							!
-						</p>
-					<h3>Membership Portal</h3>
-						<p className={styles['project-info']}>The&nbsp;
-							<Link href='https://members.uclaacm.com/'>
-								<a target='_blank' rel='noopener noreferrer'>Membership Portal</a>
-							</Link>
-							{/* eslint-disable-next-line max-len */}
-							&nbsp;is where ACM members can check in to events to earn points! We built the Membership Portal with Node.js, Express.js, PostgreSQL, Redux and React.js.
-						</p>
-					<h3>Discord Bot</h3>
-						<p className={styles['project-info']}>The Discord bot is our in-house email verification bot for the ACM at UCLA&nbsp;
-							<Link href='https://discord.gg/eWmzKsY'>
-								<a target='_blank' rel='noopener noreferrer'>Discord</a>
-							</Link>
-							&nbsp;server! It&apos;s written in JavaScript using the Discord API and Amazon SES.
-						</p>
-					<h3>Link Shortener</h3>
-						<p className={styles['project-info']}>The&nbsp;
-							<Link href='http://links.uclaacm.com/'>
-								<a target='_blank' rel='noopener noreferrer'>link shortener</a>
-							</Link>
-							{/* eslint-disable-next-line max-len */}
-							&nbsp;is an ongoing project aimed at generating shortened links for use by our committees. The link shortener is built with Flask.</p>
-					<h3>???</h3>
-						<p className={styles['project-info']}>Have an idea for a project or any questions? Feel free to contact <a href={`mailto:${devTeamDirector.email}`}>{`${devTeamDirector.name} (${devTeamDirector.email})`}</a>, our Dev Team Director.</p>
-
+				<h2 className='text-center'>Our Projects</h2>
+				<div className='grid-desktop-3 text-center-mobile'>
+					<Projects projects={projects} />
 				</div>
-				<h2 className="text-center">Our Commitment to Open Source</h2>
+				<h2 className='text-center'>Our Commitment to Open Source</h2>
 				<p className={styles['project-info']}>
 					Everything we build is&nbsp;
 					<Link href='https://opensource.uclaacm.com/'>
@@ -96,8 +61,8 @@ function DevTeam() {
 						<a target='_blank' rel='noopener noreferrer'>Github</a>
 					</Link>.
 				</p>
-				<h2 className="text-center">People</h2>
-				<div className="grid-desktop-3 text-center-mobile">
+				<h2 className='text-center'>People</h2>
+				<div className='grid-desktop-3 text-center-mobile'>
 					<Officers officers={devTeamOfficers} />
 				</div>
 			</div>
