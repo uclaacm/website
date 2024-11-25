a12fe8f41d11e7ce8189e8e6f6963fa674e18152
smalex-z <alexzheng2004@gmail.com>
2024-11-25 22:35:59 +0000

    description update

--- pages/dev.js
+++ pages/dev.js
@@ -31,14 +31,14 @@ function DevTeam() {
 		<Layout>
 			<NextSeo
 				title='ACM Dev Team | ACM at UCLA'
-				description='The ACM Dev Team handles general internal development needs for ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and link shortener.'
+				description='The ACM Dev Team handles general internal development needs for ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and CMS Template.'
 			/>
 			<Banner decorative />
 			<div className={'content-section text-center'}>
 				<h1 className='text-center'>Dev Team</h1>
 				<p className={styles['dev-team-info']}>
 					{/* eslint-disable-next-line max-len */}
-					The ACM Dev Team handles general internal development needs for all of ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and link shortener.
+					The ACM Dev Team handles general internal development needs for all of ACM at UCLA. We maintain and create organization-wide projects such as the website, Discord bot, Membership Portal, and CMS Template.
 				</p>
 				<p className={styles['dev-team-info']}>
 					If you&apos;re interested in joining us,&nbsp;
