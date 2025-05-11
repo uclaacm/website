bea951e945b5e49aafee5225d9bc100ea332e5a7
Nathan Zhang <nml707805@gmail.com>
2025-05-10 18:19:18 -0700

    More important linting fixes

--- __tests__/components/NewsArticle.test.js
+++ __tests__/components/NewsArticle.test.js
@@ -3,12 +3,12 @@ import { axe, toHaveNoViolations } from 'jest-axe';
 
 import { act } from 'react';
 import NewsArticle from '../../components/NewsArticle';
-import data from '../../data';
+import news from '../newsoutput.json';
 
 expect.extend(toHaveNoViolations);
 
 it('has no axe violations', async () => {
-  const article = data.news[0];
+  const article = news[0];
   // this let / async construct is required since next/image rendering
   // is a side effect that needs to be captured with async act
   // see https://reactjs.org/docs/test-utils.html#act
