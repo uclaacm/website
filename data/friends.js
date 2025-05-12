49bce983a2832151857fdf9728beda5a995edb3a
pegifenu <110061199+pegifenu@users.noreply.github.com>
2025-05-12 01:44:04 -0700

    Feature: New Friends Page (#806)
    * Added basic friends files (page, data, images, styles) and linked page in footer.
    
    * almost finished css layout and started populating data/friends.js with real data
    
    * more styling polish and populated all available data
    
    * friends container max width and other style polish
    
    * prevented view link from taking up whole box
    
    * fixed eslint errors and warnings
    
    * added creative labs desc
    
    * fixed typo
    
    * fixed the actual type lmao

---
+++ data/friends.js
@@ -0,0 +1,164 @@
+export default [
+  {
+    name: 'UPE',
+    image: '/images/friends/upe.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://upe.seas.ucla.edu/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/upeucla',
+      },
+      {
+        platform: 'discord',
+        link: 'https://tinyurl.com/upe-discord',
+      },
+    ],
+    description:
+      'UPE has been a key partner in hosting a variety of events, including town halls and career fairs, enhancing our community\'s professional development and networking opportunities.',
+    projects: [
+      {
+        title: 'Town Hall',
+        description:
+          'The CS Town Hall is an opportunity for students to directly speak with professors and administrators in the CS department where they can provide feedback to improve curriculum.',
+        image: '',
+        url: 'https://acm.cs.ucla.edu/town-hall',
+      },
+      {
+        title: 'CS Career Fair',
+        description:
+          'The CS Career Fair allows students to explore career paths, connect with industry professionals, and learn about job and internship opportunities directly from affiliated company representatives.',
+        image: '',
+        url: 'https://www.cs.ucla.edu/upcoming-events/upe-honors-fair-computer-science-career-fair/',
+      },
+    ],
+  },
+  {
+    name: 'exploretech.la',
+    image: '/images/friends/exploretechla.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://www.exploretech.la/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/exploretech.la',
+      },
+    ],
+    description:
+      'Our collaborative efforts with exploretech.la have brought exciting exhibitions and opportunities for our members to explore the latest in technology and innovation.',
+    projects: [
+      {
+        title: 'Town Hall',
+        description:
+          'The CS Town Hall is an opportunity for students to directly speak with professors and administrators in the CS department where they can provide feedback to improve curriculum.',
+        image: '',
+        url: 'https://acm.cs.ucla.edu/town-hall',
+      },
+    ],
+  },
+  {
+    name: 'Creative Labs',
+    image: '/images/friends/creativelabs.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://www.creativelabsucla.com/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/creativelabsucla',
+      },
+      {
+        platform: 'discord',
+        link: 'https://discord.gg/vHenfGNTXJ',
+      },
+    ],
+    description:
+      'Creative Labs has been a partner in the past for various projects - take a look at the example below!',
+    projects: [
+      {
+        title: 'Playnet',
+        description:
+          '',
+        image: '',
+        url: 'https://playnet.uclaacm.com/',
+      },
+    ],
+  },
+  {
+    name: 'BEAM',
+    image: '/images/friends/beam.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://beam.ucla.edu/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/beam_ucla',
+      },
+    ],
+    description:
+      'BEAM has been a vital collaborator with Teach LA, co-hosting cross-teaching sessions that enrich learning experiences and foster skill-building within our community.',
+    projects: [
+      {
+        title: 'Robotics Fun with BEAM',
+        description:
+          'Robotics Fun with BEAM was a seven-week program where ACM Teach LA and BEAM introduced 4th and 5th graders to robotics, coding, and hands-on assembly with mBots!',
+        image: '',
+        url: 'https://teachla.uclaacm.com/beam-robotics',
+      },
+    ],
+  },
+  {
+    name: 'LA Hacks',
+    image: '/images/friends/lahacks.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://lahacks.com/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/lahacks',
+      },
+    ],
+    description:
+      'Formerly part of ACM, LA Hacks now collaborates with us on workshops, continuing a tradition of fostering innovation and technical excellence.',
+    projects: [],
+  },
+  {
+    name: 'QWER Hacks',
+    image: '/images/friends/qwerhacks.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://www.qwerhacks.com/',
+      },
+      {
+        platform: 'instagram',
+        link: 'https://www.instagram.com/qwerhacks',
+      },
+    ],
+    description:
+      'QWER Hacks and our organization share founding members and a commitment to diversity and inclusion, working together on impactful workshops and events.',
+    projects: [],
+  },
+  {
+    name: 'ACM at UCSD',
+    image: '/images/friends/acm_ucsd.png',
+    links: [
+      {
+        platform: 'website',
+        link: 'https://acmucsd.com/',
+      },
+    ],
+    description:
+      'ACM at UCSD has been a close ally, supporting a wide range of activities and initiatives as a prominent SoCal friend and spin-off from our own organization.',
+    projects: [],
+  },
+];
