e0a0167c93c96ae96eef8209995b2b58f670ceea
daanniill <daniil.malakhov12@gmail.com>
2026-03-07 19:01:30 -0800

    fixed linter warning in quantum.js

--- pages/quantum.js
+++ pages/quantum.js
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextSeo } from 'next-seo';
 import Layout from '../components/Layout';
 import styles from '../styles/pages/quantum.module.scss';
